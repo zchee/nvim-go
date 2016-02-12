3db7a9e89ee1b2195437eed89b7f18ce43915f87
Koichi Shiraishi <k@zchee.io>
2016-02-13 03:08:08 +0900

    Fix Echo*** and Add ReportError interface wrapper
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/nvim/command.go
+++ src/nvim-go/nvim/command.go
@@ -6,10 +6,18 @@ import (
 	"github.com/garyburd/neovim-go/vim"
 )
 
-func Echomsg(v *vim.Vim, format string, args ...interface{}) error {
-	return v.WriteOut(fmt.Sprintf(format, args...))
+func Echo(v *vim.Vim, format string, a ...interface{}) error {
+	return v.Command("echo '" + fmt.Sprintf(format, a...) + "'")
 }
 
-func Echoerr(v *vim.Vim, format string, args ...interface{}) error {
-	return v.WriteErr(fmt.Sprintf(format, args...))
+func Echomsg(v *vim.Vim, format string, a ...interface{}) error {
+	return v.Command("echomsg '" + fmt.Sprintf(format, a...) + "'")
+}
+
+func Echoerr(v *vim.Vim, format string, a ...interface{}) error {
+	return v.Command("echoerr '" + fmt.Sprintf(format, a...) + "'")
+}
+
+func ReportError(v *vim.Vim, format string, a ...interface{}) error {
+	return v.ReportError(fmt.Sprintf(format, a...))
 }
