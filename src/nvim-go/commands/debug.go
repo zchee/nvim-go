3326b013b5a4471b3d75bfbe0b4ddb4af4f6d12f
Koichi Shiraishi <k@zchee.io>
2016-02-13 05:38:02 +0900

    Fix debug.go
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/commands/debug.go
+++ src/nvim-go/commands/debug.go
@@ -1,9 +1,6 @@
 package commands
 
 import (
-	"encoding/binary"
-	"fmt"
-
 	"nvim-go/nvim"
 
 	"github.com/garyburd/neovim-go/vim"
@@ -15,36 +12,19 @@ func init() {
 }
 
 func Debug(v *vim.Vim, args []string, cwd string) error {
-	b, err := v.CurrentBuffer()
-	if err != nil {
-		return err
-	}
-	bu, err := v.BufferLineSlice(b, 0, -1, true, true)
-	if err != nil {
-		return err
-	}
-
 	w, _ := v.CurrentWindow()
-	cursor, _ := v.WindowCursor(w)
+	// b, _ := v.CurrentBuffer()
+	// l, _ := v.CurrentLine()
+	// t, _ := v.CurrentTabpage()
+
+	// var vars interface{}
+	// v.Var("go#test#vars", &vars)
+	// fmt.Println(vars)
+	// return nvim.Echomsg(v, "loaded")
+
+	win, _ := v.Windows()
 	height, _ := v.WindowHeight(w)
 	width, _ := v.WindowWidth(w)
 	pos, _ := v.WindowPosition(w)
-	win, _ := v.Windows()
-
-	offset := 0
-	cursorline := 1
-	for _, bytes := range bu {
-		if cursor[0] == 1 {
-			offset = 1
-			break
-		} else if cursorline == cursor[0] {
-			offset++
-			break
-		}
-		offset += (binary.Size(bytes) + 1)
-		cursorline++
-	}
-
-	// return command.Echomsg(v, fmt.Sprintf("line: %d col: %d offset: %d", cursor[0], cursor[1], (offset+(cursor[1]-1))))
-	return nvim.Echomsg(v, fmt.Sprintf("win: %v height: %v width: %v pos %v", win, height, width, pos))
+	return nvim.Echomsg(v, "win: %v height: %v width: %v pos %v", win, height, width, pos)
 }
