f43f279ee07409370962fd74032846dea5a40a8c
Koichi Shiraishi <k@zchee.io>
2016-04-22 15:50:57 +0900

    Remove 'redraw!' before echo command
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/nvim/echo.go
+++ src/nvim-go/nvim/echo.go
@@ -12,31 +12,26 @@ import (
 
 // Echo provide the vim 'echo' command.
 func Echo(v *vim.Vim, format string, a ...interface{}) error {
-	v.Command("redraw!")
 	return v.Command("echo '" + fmt.Sprintf(format, a...) + "'")
 }
 
 // Echomsg provide the vim 'echomsg' command.
 func Echomsg(v *vim.Vim, a ...interface{}) error {
-	v.Command("redraw!")
 	return v.Command("echomsg '" + fmt.Sprintln(a...) + "'")
 }
 
 // Echoerr provide the vim 'echoerr' command.
 func Echoerr(v *vim.Vim, a ...interface{}) error {
-	v.Command("redraw!")
 	return v.Command("echoerr '" + fmt.Sprintln(a...) + "'")
 }
 
 // Echohl provide the vim 'echohl' command with message prefix and message color highlighting.
 // Nomally, used to output the any command results.
 func Echohl(v *vim.Vim, prefix string, highlight string, format string, a ...interface{}) error {
-	v.Command("redraw!")
 	return v.Command("echo '" + prefix + "' | echohl " + highlight + " | echon '" + fmt.Sprintf(format, a...) + "' | echohl None")
 }
 
 // ReportError output of the accumulated errors report.
 func ReportError(v *vim.Vim, format string, a ...interface{}) error {
-	v.Command("redraw!")
 	return v.ReportError(fmt.Sprintf(format, a...))
 }
