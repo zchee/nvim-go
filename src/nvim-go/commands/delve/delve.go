113003f9e4f8349356fb62dca3da6bde97436f16
Koichi Shiraishi <k@zchee.io>
2016-05-17 01:29:42 +0900

    delve: Split regiter commmand function
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/commands/delve/delve.go
+++ src/nvim-go/commands/delve/delve.go
@@ -21,7 +21,6 @@ import (
 	delverpc2 "github.com/derekparker/delve/service/rpc2"
 	delveterminal "github.com/derekparker/delve/terminal"
 	"github.com/garyburd/neovim-go/vim"
-	"github.com/garyburd/neovim-go/vim/plugin"
 )
 
 const addr = "localhost:41222" // d:4 l:12 v:22
@@ -76,70 +75,6 @@ func NewDelveClient(addr string) *DelveClient {
 	return &DelveClient{
 		addr: addr,
 	}
-}
-
-func init() {
-	// Launch
-	plugin.HandleCommand("DlvStartServer", &plugin.CommandOptions{NArgs: "*", Eval: "[getcwd(), expand('%:p:h')]", Complete: "file"}, cmdDelveStartServer)
-	plugin.HandleCommand("DlvStartClient", &plugin.CommandOptions{Eval: "[getcwd(), expand('%:p:h')]"}, delveStartClient)
-
-	// Command
-	plugin.HandleCommand("DlvContinue", &plugin.CommandOptions{}, cmdDelveContinue)
-	plugin.HandleCommand("DlvNext", &plugin.CommandOptions{}, cmdDelveNext)
-	plugin.HandleCommand("DlvRestart", &plugin.CommandOptions{}, cmdDelveRestart)
-	plugin.HandleCommand("DlvDisassemble", &plugin.CommandOptions{}, delveDisassemble)
-	plugin.HandleCommand("DlvCommand", &plugin.CommandOptions{NArgs: "+"}, cmdDelveCommand)
-
-	// Breokpoint
-	plugin.HandleCommand("DlvBreakpoint", &plugin.CommandOptions{NArgs: "+", Complete: "customlist,DelveFunctionList"}, delveBreakpoint)
-	plugin.HandleFunction("DelveFunctionList", &plugin.FunctionOptions{}, delveFunctionList)
-
-	// RPC export
-	plugin.Handle("DlvContinue", cmdDelveContinue)
-	plugin.Handle("DlvNext", cmdDelveNext)
-	plugin.Handle("DlvRestart", cmdDelveRestart)
-	plugin.Handle("DlvDetach", CmdDelveDetach)
-
-	// Exit
-	plugin.HandleCommand("DlvDetach", &plugin.CommandOptions{}, CmdDelveDetach)
-	plugin.HandleCommand("DlvKill", &plugin.CommandOptions{}, CmdDelveKill)
-}
-
-// cmdBuildEval represent a Dlv commands Eval args.
-type cmdDelveEval struct {
-	Cwd string `msgpack:",array"`
-	Dir string
-}
-
-// Wrapper function for commands using goroutine.
-//
-// The advantage is do not freeze the neovim user interface even if any command resulting the busy state.
-// Note may become multistage concurrency processing.
-//
-//  Neovim rpc call (asynchronous)
-//    -> Wrapper function (goroutine)
-//      -> Remote plugin internal (goroutine)
-//        -> neovim-go/vim.Pipeline (goroutine & chan)
-func cmdDelveStartServer(v *vim.Vim, args []string, eval cmdDelveEval) {
-	go delveStartServer(v, args, eval)
-}
-func cmdDelveCommand(v *vim.Vim, args []string) {
-	go delveCommand(v, args)
-}
-func cmdDelveContinue(v *vim.Vim) {
-	go delveContinue(v)
-}
-func cmdDelveNext(v *vim.Vim) {
-	go delveNext(v)
-}
-func cmdDelveRestart(v *vim.Vim) {
-	go delveRestart(v)
-}
-func CmdDelveDetach(v *vim.Vim) {
-	go delveDetach(v)
-}
-func CmdDelveKill(v *vim.Vim) {
-	go delveKill()
 }
 
 // startServer starts the delve headless server and hijacked stdout & stderr.
---
+++ src/nvim-go/commands/delve/init.go
@@ -0,0 +1,70 @@
+package delve
+
+import (
+	"github.com/garyburd/neovim-go/vim"
+	"github.com/garyburd/neovim-go/vim/plugin"
+)
+
+func init() {
+	// Launch
+	plugin.HandleCommand("DlvStartServer", &plugin.CommandOptions{NArgs: "*", Eval: "[getcwd(), expand('%:p:h')]", Complete: "file"}, cmdDelveStartServer)
+	plugin.HandleCommand("DlvStartClient", &plugin.CommandOptions{Eval: "[getcwd(), expand('%:p:h')]"}, delveStartClient)
+
+	// Command
+	plugin.HandleCommand("DlvContinue", &plugin.CommandOptions{}, cmdDelveContinue)
+	plugin.HandleCommand("DlvNext", &plugin.CommandOptions{}, cmdDelveNext)
+	plugin.HandleCommand("DlvRestart", &plugin.CommandOptions{}, cmdDelveRestart)
+	plugin.HandleCommand("DlvDisassemble", &plugin.CommandOptions{}, delveDisassemble)
+	plugin.HandleCommand("DlvCommand", &plugin.CommandOptions{NArgs: "+"}, cmdDelveCommand)
+
+	// Breokpoint
+	plugin.HandleCommand("DlvBreakpoint", &plugin.CommandOptions{NArgs: "+", Complete: "customlist,DelveFunctionList"}, delveBreakpoint)
+	plugin.HandleFunction("DelveFunctionList", &plugin.FunctionOptions{}, delveFunctionList)
+
+	// RPC export
+	plugin.Handle("DlvContinue", cmdDelveContinue)
+	plugin.Handle("DlvNext", cmdDelveNext)
+	plugin.Handle("DlvRestart", cmdDelveRestart)
+	plugin.Handle("DlvDetach", CmdDelveDetach)
+
+	// Exit
+	plugin.HandleCommand("DlvDetach", &plugin.CommandOptions{}, CmdDelveDetach)
+	plugin.HandleCommand("DlvKill", &plugin.CommandOptions{}, CmdDelveKill)
+}
+
+// cmdBuildEval represent a Dlv commands Eval args.
+type cmdDelveEval struct {
+	Cwd string `msgpack:",array"`
+	Dir string
+}
+
+// Wrapper function for commands using goroutine.
+//
+// The advantage is do not freeze the neovim user interface even if any command resulting the busy state.
+// Note may become multistage concurrency processing.
+//
+//  Neovim rpc call (asynchronous)
+//    -> Wrapper function (goroutine)
+//      -> Remote plugin internal (goroutine)
+//        -> neovim-go/vim.Pipeline (goroutine & chan)
+func cmdDelveStartServer(v *vim.Vim, args []string, eval cmdDelveEval) {
+	go delveStartServer(v, args, eval)
+}
+func cmdDelveCommand(v *vim.Vim, args []string) {
+	go delveCommand(v, args)
+}
+func cmdDelveContinue(v *vim.Vim) {
+	go delveContinue(v)
+}
+func cmdDelveNext(v *vim.Vim) {
+	go delveNext(v)
+}
+func cmdDelveRestart(v *vim.Vim) {
+	go delveRestart(v)
+}
+func CmdDelveDetach(v *vim.Vim) {
+	go delveDetach(v)
+}
+func CmdDelveKill(v *vim.Vim) {
+	go delveKill()
+}
