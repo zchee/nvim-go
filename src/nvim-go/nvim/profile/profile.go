3baa6671e64e81aa95ebf3b617dac870fe694a09
Koichi Shiraishi <k@zchee.io>
2016-05-21 01:01:48 +0900

    nvim/profile: Change profile measure of time to second instead of ms
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/nvim/profile/profile.go
+++ src/nvim-go/nvim/profile/profile.go
@@ -8,6 +8,6 @@ import (
 // Profile measurement of the time it took to any func and output log file.
 // Usage: defer nvim.Profile(time.Now(), "func name")
 func Start(start time.Time, name string) {
-	elapsed := time.Since(start)
-	log.Printf("%s: %s\n", name, elapsed)
+	elapsed := time.Since(start).Seconds()
+	log.Printf("%s: %fsec\n", name, elapsed)
 }
