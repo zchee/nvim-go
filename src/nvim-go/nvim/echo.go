e1533b454c7ec2024b5fcbaed62f2a8849166e71
Koichi Shiraishi <k@zchee.io>
2016-05-22 01:43:42 +0900

    nvim/echo: Fix clear echo area to use redraw instead of echon
    - echon sometimes does not clear the echo area
    
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/nvim/echo.go
+++ src/nvim-go/nvim/echo.go
@@ -22,32 +22,32 @@ var (
 
 // Echo provide the vim 'echo' command.
 func Echo(v *vim.Vim, format string, a ...interface{}) error {
-	v.Command("echon")
+	v.Command("redraw")
 	return v.Command("echo '" + fmt.Sprintf(format, a...) + "'")
 }
 
 // EchoRaw provide the raw output vim 'echo' command.
 func EchoRaw(v *vim.Vim, a string) error {
-	v.Command("echon")
+	v.Command("redraw")
 	return v.Command("echo \"" + a + "\"")
 }
 
 // Echomsg provide the vim 'echomsg' command.
 func Echomsg(v *vim.Vim, a ...interface{}) error {
-	v.Command("echon")
+	v.Command("redraw")
 	return v.Command("echomsg '" + strings.TrimSpace(fmt.Sprintln(a...)) + "'")
 }
 
 // Echoerr provide the vim 'echoerr' command.
 func Echoerr(v *vim.Vim, format string, a ...interface{}) error {
-	v.Command("echon")
+	v.Command("redraw")
 	return v.Command("echoerr '" + fmt.Sprintf(format, a...) + "'")
 }
 
 // ErrorWrap splits the errors.Annotate's cause and error messages,
 // and provide the vim 'echo' message with 'echohl' highlighting to cause text.
 func ErrorWrap(v *vim.Vim, err error) error {
-	v.Command("echon")
+	v.Command("redraw")
 	er := strings.SplitAfterN(fmt.Sprintf("%s", err), ": ", 2)
 	if os.Getenv("NVIM_GO_DEBUG") != "" {
 		log.Printf("Error stack\n%s", errors.ErrorStack(err))
@@ -57,7 +57,7 @@ func ErrorWrap(v *vim.Vim, err error) error {
 
 // EchohlErr provide the vim 'echo' command with the 'echohl' highlighting prefix text.
 func EchohlErr(v *vim.Vim, prefix string, a ...interface{}) error {
-	v.Command("echon")
+	v.Command("redraw")
 	if prefix != "" {
 		prefix += ": "
 	}
@@ -67,7 +67,7 @@ func EchohlErr(v *vim.Vim, prefix string, a ...interface{}) error {
 
 // EchohlBefore provide the vim 'echo' command with the 'echohl' highlighting prefix text.
 func EchohlBefore(v *vim.Vim, prefix string, highlight string, format string, a ...interface{}) error {
-	v.Command("echon")
+	v.Command("redraw")
 	suffix := "' | echohl None | echon '"
 	if prefix != "" {
 		suffix += ": "
@@ -77,7 +77,7 @@ func EchohlBefore(v *vim.Vim, prefix string, highlight string, format string, a 
 
 // EchohlAfter provide the vim 'echo' command with the 'echohl' highlighting message text.
 func EchohlAfter(v *vim.Vim, prefix string, highlight string, format string, a ...interface{}) error {
-	v.Command("echon")
+	v.Command("redraw")
 	if prefix != "" {
 		prefix += ": "
 	}
@@ -86,14 +86,14 @@ func EchohlAfter(v *vim.Vim, prefix string, highlight string, format string, a .
 
 // EchoProgress displays a command progress message to echo area.
 func EchoProgress(v *vim.Vim, prefix, format string, a ...interface{}) error {
-	v.Command("echon")
+	v.Command("redraw")
 	msg := fmt.Sprintf(format, a...)
 	return v.Command(fmt.Sprintf("echo \"%s: \" | echohl %s | echon \"%s ...\" | echohl None", prefix, progress, msg))
 }
 
 // EchoSuccess displays the success of the command to echo area.
 func EchoSuccess(v *vim.Vim, prefix string, msg string) error {
-	v.Command("echon")
+	v.Command("redraw")
 	if msg != "" {
 		msg = " - " + msg
 	}
