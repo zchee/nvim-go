38b9dc512990edf4e06a0a0f7fab8f533f21a6d3
Koichi Shiraishi <k@zchee.io>
2016-05-26 18:29:34 +0900

    nvim/terminal: Fix use wrong buffer and buffer init to use buffer pkg
    - Fix: if closed terminal buffer, next buffer open had been used by
      the current buffer
    
    - Change buffer initialize to use buffer.NewBuffer() function
    
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/nvim/terminal/terminal.go
+++ src/nvim-go/nvim/terminal/terminal.go
@@ -6,21 +6,20 @@ package terminal
 
 import (
 	"fmt"
-	"log"
-	"strconv"
 	"strings"
 	"sync"
 
 	"nvim-go/config"
 	"nvim-go/nvim"
+	"nvim-go/nvim/buffer"
+	"nvim-go/pathutil"
 
 	"github.com/garyburd/neovim-go/vim"
 )
 
-var (
-	tbuffer vim.Buffer
-	twindow vim.Window
-)
+var pkgTerminal = "GoTerminal"
+
+var bufName = "__GO_TERMINAL__"
 
 // Terminal configure of open the terminal.
 type Terminal struct {
@@ -33,17 +32,11 @@ type Terminal struct {
 	Width int64
 	// Height split window height for open the terminal window.
 	Height int64
+	Size   int
 
-	Filetype  string
-	Buftype   string
-	Bufhidden string
-	Buflisted bool
-	Swapfile  bool
-
-	List           bool
-	Number         bool
-	Relativenumber bool
-	Winfixheight   bool
+	cb     vim.Buffer
+	cw     vim.Window
+	Buffer *buffer.Buffer
 }
 
 // NewTerminal return the initialize Neovim terminal config.
@@ -52,152 +45,121 @@ func NewTerminal(vim *vim.Vim, command []string, mode string) *Terminal {
 		v:    vim,
 		cmd:  command,
 		mode: mode,
-
-		Filetype:  "terminal",
-		Buftype:   "nofile",
-		Bufhidden: "delete",
-		Buflisted: false,
-		Swapfile:  false,
-
-		List:           false,
-		Number:         false,
-		Relativenumber: false,
-		Winfixheight:   true,
 	}
+}
+
+func (t *Terminal) Create() error {
+	p := t.v.NewPipeline()
+	p.CurrentBuffer(&t.cb)
+	p.CurrentWindow(&t.cw)
+	if err := p.Wait(); err != nil {
+		return err
+	}
+
+	t.Height = config.TerminalHeight
+	t.Width = config.TerminalWidth
+
+	switch {
+	case t.Height != int64(0) && t.mode == "split":
+		t.Size = int(t.Height)
+	case t.Width != int64(0) && t.mode == "vsplit":
+		t.Size = int(t.Width)
+	case strings.Index(t.mode, "split") == -1:
+		return fmt.Errorf("%s mode is not supported", t.mode)
+	}
+
+	t.Buffer = buffer.NewBuffer(fmt.Sprintf("| terminal %s", strings.Join(t.cmd, " ")), fmt.Sprintf("%s %d%s", config.TerminalPosition, t.Size, t.mode), t.Size)
+	t.Buffer.Create(t.v, t.setTerminalOption("buffer"), t.setTerminalVar("buffer"), t.setTerminalOption("window"), t.setTerminalVar("window"))
+
+	// Get terminal buffer and windows information.
+	p.CurrentBuffer(&t.Buffer.Buffer)
+	p.CurrentWindow(&t.Buffer.Window)
+	if err := p.Wait(); err != nil {
+		return err
+	}
+
+	// Cleanup cursor highlighting
+	// TODO(zchee): Can use p.ClearBufferHighlight?
+	p.Command("highlight TermCursor gui=NONE guifg=NONE guibg=NONE")
+	p.Command("highlight TermCursorNC gui=NONE guifg=NONE guibg=NONE")
+
+	// Cleanup autocmd for terminal buffer
+	// The following autocmd is defined only in the terminal buffer local
+	p.Command("autocmd! * <buffer>")
+	// Set autoclose buffer if the current buffer is only terminal
+	// TODO(zchee): convert to rpc way
+	p.Command("autocmd WinEnter <buffer> if winnr('$') == 1 | quit | endif")
+
+	return p.Wait()
 }
 
 // Run runs the command in the terminal buffer.
 func (t *Terminal) Run() error {
 	if t.Dir != "" {
-		defer chdir(t.v, t.Dir)()
+		defer pathutil.Chdir(t.v, t.Dir)()
 	}
 
-	var (
-		b      vim.Buffer
-		w      vim.Window
-		pos    = config.TerminalPosition
-		height = config.TerminalHeight
-		width  = config.TerminalWidth
-	)
+	if t.Buffer != nil && buffer.Contains(t.v, t.Buffer.Buffer) {
+		defer t.switchFocus()()
 
-	// Creates a new pipeline
-	p := t.v.NewPipeline()
-	p.CurrentBuffer(&b)
-	p.CurrentWindow(&w)
-	if err := p.Wait(); err != nil {
-		return err
-	}
-
-	if twindow != 0 {
-		p.SetCurrentWindow(twindow)
-		p.SetBufferOption(tbuffer, "modified", false)
-		p.Call("termopen", nil, strings.Join(t.cmd, " "))
-		p.SetBufferOption(tbuffer, "modified", true)
+		t.v.SetBufferOption(t.Buffer.Buffer, buffer.OpModified, false)
+		t.v.Call("termopen", nil, t.cmd)
 	} else {
-		// Set split window position. (defalut: botright)
-		vcmd := pos + " "
-
-		t.Height = height
-		t.Width = width
-
-		switch {
-		case t.Height != int64(0) && t.mode == "split":
-			vcmd += strconv.FormatInt(t.Height, 10)
-		case t.Width != int64(0) && t.mode == "vsplit":
-			vcmd += strconv.FormatInt(t.Width, 10)
-		case strings.Index(t.mode, "split") == -1:
-			return fmt.Errorf("%s mode is not supported", t.mode)
-		}
-
-		// Create terminal buffer and spawn command.
-		vcmd += t.mode + " | terminal " + strings.Join(t.cmd, " ")
-		p.Command(vcmd)
-
-		// Get terminal buffer and windows information.
-		p.CurrentBuffer(&tbuffer)
-		p.CurrentWindow(&twindow)
-		if err := p.Wait(); err != nil {
-			return err
-		}
-
-		// Workaround for "autocmd BufEnter term://* startinsert"
-		if config.TerminalStartInsert {
-			p.Command("stopinsert")
-		}
-
-		p.SetBufferOption(tbuffer, "filetype", t.Filetype)
-		p.SetBufferOption(tbuffer, "buftype", t.Buftype)
-		p.SetBufferOption(tbuffer, "bufhidden", t.Bufhidden)
-		p.SetBufferOption(tbuffer, "buflisted", t.Buflisted)
-		p.SetBufferOption(tbuffer, "swapfile", t.Swapfile)
-
-		p.SetWindowOption(twindow, "list", t.List)
-		p.SetWindowOption(twindow, "number", t.Number)
-		p.SetWindowOption(twindow, "relativenumber", t.Relativenumber)
-		p.SetWindowOption(twindow, "winfixheight", t.Winfixheight)
-
-		// Cleanup cursor highlighting
-		// TODO(zchee): Can use p.AddBufferHighlight?
-		p.Command("hi TermCursor gui=NONE guifg=NONE guibg=NONE")
-		p.Command("hi! TermCursorNC gui=NONE guifg=NONE guibg=NONE")
-
-		// Cleanup autocmd for terminal buffer
-		// The following autocmd is defined only in the terminal buffer local
-		p.Command("autocmd! * <buffer>")
-		// Set autocmd of automatically insert mode
-		p.Command("autocmd WinEnter <buffer> startinsert")
-		// Set autoclose buffer if the current buffer is only terminal
-		// TODO(zchee): convert to rpc way
-		p.Command("autocmd WinEnter <buffer> if winnr('$') == 1 | quit | endif")
+		t.Create()
+		defer t.switchFocus()()
+	}
+	// Workaround for "autocmd BufEnter term://* startinsert"
+	if config.TerminalStartInsert {
+		t.v.Command("stopinsert")
 	}
 
-	// Set buffer name, filetype and options
-	p.SetBufferName(tbuffer, "__GO_TERMINAL__")
-
-	// Refocus coding buffer and stop insert mode
-	p.SetCurrentWindow(w)
-	p.Command("stopinsert")
-
-	return p.Wait()
+	return nil
 }
 
-func Command(v *vim.Vim, w vim.Window, command string) error {
-	defer switchFocus(v, w)()
+func (t *Terminal) Command(command string) error {
+	defer t.switchFocus()()
+	t.v.FeedKeys("i"+command+"\r", "n", true)
+	return t.v.Command("stopinsert")
+}
 
-	var b vim.Buffer
-	p := v.NewPipeline()
-	p.CurrentBuffer(&b)
-	p.SetBufferOption(b, "modified", false)
-	p.Wait()
+func (t *Terminal) setTerminalOption(scope string) map[string]interface{} {
+	options := make(map[string]interface{})
 
-	p.FeedKeys("i"+command+"\r", "n", true)
-	p.SetBufferOption(b, "modified", true)
-	log.Printf("command: %+v\n", command)
-	p.Command("stopinsert")
+	switch scope {
+	case "buffer":
+		options[buffer.Bufhidden] = buffer.BufhiddenDelete
+		options[buffer.Buflisted] = false
+		options[buffer.Buftype] = buffer.BuftypeNofile
+		options[buffer.Filetype] = buffer.FiletypeTerminal
+		options[buffer.OpModifiable] = false
+		options[buffer.Swapfile] = false
+	case "window":
+		options[buffer.List] = false
+		options[buffer.Number] = false
+		options[buffer.Relativenumber] = false
+		options[buffer.Winfixheight] = true
+	}
 
-	return p.Wait()
+	return options
+}
+
+func (t *Terminal) setTerminalVar(scope string) map[string]interface{} {
+	vars := make(map[string]interface{})
+
+	switch scope {
+	case "buffer":
+		vars[buffer.Colorcolumn] = ""
+	}
+
+	return vars
 }
 
 // TODO(zchee): flashing when switch the window.
-func switchFocus(v *vim.Vim, w vim.Window) func() {
-	var (
-		m  sync.Mutex
-		cw vim.Window
-	)
-	m.Lock()
-
-	p := v.NewPipeline()
-	p.CurrentWindow(&cw)
-
-	if err := p.Wait(); err != nil {
-		nvim.Echoerr(v, "GoTerminal: %v", err)
-	}
-
-	p.SetCurrentWindow(w)
+func (t *Terminal) switchFocus() func() {
+	t.v.SetCurrentWindow(t.Buffer.Window)
 
 	return func() {
-		v.SetCurrentWindow(cw)
-		m.Unlock()
+		t.v.SetCurrentWindow(t.cw)
 	}
 }
 
