b06d7d2ccd1e334583d48232e6962a4d5530761d
Koichi Shiraishi <k@zchee.io>
2016-05-26 18:23:48 +0900

    util: Add list of Buffer, Window, Tabpage debug command
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/commands/util.go
+++ src/nvim-go/commands/util.go
@@ -16,6 +16,36 @@ func init() {
 	plugin.HandleCommand("GoByteOffset",
 		&plugin.CommandOptions{Range: "%", Eval: "expand('%:p')"},
 		cmdByteOffset)
+	plugin.HandleCommand("GoBuffers", &plugin.CommandOptions{}, cmdBuffers)
+	plugin.HandleCommand("GoWindows", &plugin.CommandOptions{}, cmdWindows)
+	plugin.HandleCommand("GoTabpages", &plugin.CommandOptions{}, cmdTabpagas)
+}
+
+func cmdBuffers(v *vim.Vim) error {
+	bufs, _ := v.Buffers()
+	var b []string
+	for _, buf := range bufs {
+		b = append(b, buf.String())
+	}
+	return nvim.Echomsg(v, "Buffers:", b)
+}
+
+func cmdWindows(v *vim.Vim) error {
+	wins, _ := v.Windows()
+	var w []string
+	for _, win := range wins {
+		w = append(w, win.String())
+	}
+	return nvim.Echomsg(v, "Windows:", w)
+}
+
+func cmdTabpagas(v *vim.Vim) error {
+	tabs, _ := v.Tabpages()
+	var t []string
+	for _, tab := range tabs {
+		t = append(t, tab.String())
+	}
+	return nvim.Echomsg(v, "Tabpages:", t)
 }
 
 func cmdByteOffset(v *vim.Vim) error {
