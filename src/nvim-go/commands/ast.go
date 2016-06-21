a79c93bf10bee363c8d23bcf29a78ac4cf04878b
Koichi Shiraishi <k@zchee.io>
2016-06-21 09:45:51 +0900

    cmds/ast: Fix buffer initialization and cleanup
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/commands/ast.go
+++ src/nvim-go/commands/ast.go
@@ -12,10 +12,10 @@ import (
 	"go/token"
 	"path/filepath"
 	"time"
-	"unsafe"
 
 	"nvim-go/config"
 	"nvim-go/nvim"
+	"nvim-go/nvim/buffer"
 	"nvim-go/nvim/profile"
 
 	"github.com/garyburd/neovim-go/vim"
@@ -23,14 +23,10 @@ import (
 	"github.com/juju/errors"
 )
 
-const pkgAstView = "AstView"
-
-var (
-	astInfo []byte
-)
+const pkgAstView = "GoASTView"
 
 func init() {
-	plugin.HandleCommand("GoAstView", &plugin.CommandOptions{Eval: "[getcwd(), expand('%:p')]"}, cmdAstView)
+	plugin.HandleCommand("GoAstView", &plugin.CommandOptions{Eval: "[getcwd(), expand('%:p')]"}, cmdASTView)
 }
 
 type cmdAstEval struct {
@@ -38,84 +34,83 @@ type cmdAstEval struct {
 	File string
 }
 
-func cmdAstView(v *vim.Vim, eval *cmdAstEval) {
+func cmdASTView(v *vim.Vim, eval *cmdAstEval) {
 	go astView(v, eval)
 }
+
+type astViewer struct {
+	data []byte
+}
+
+// astBuffer global variable with cache buffer.
+var astViewBuffer *buffer.Buffer
 
 // AstView gets the Go AST informations of current buffer.
 func astView(v *vim.Vim, eval *cmdAstEval) error {
 	defer profile.Start(time.Now(), "AstView")
 
 	var (
-		b   vim.Buffer
-		w   vim.Window
-		blc int
+		b vim.Buffer
+		w vim.Window
 	)
 
 	p := v.NewPipeline()
 	p.CurrentBuffer(&b)
 	p.CurrentWindow(&w)
-	err := p.Wait()
-	if err != nil {
-		return nvim.ErrorWrap(v, errors.Annotate(err, pkgAstView))
-	}
-
-	sources := make([][]byte, blc)
-	p.BufferLines(b, 0, -1, true, &sources)
-	p.BufferLineCount(b, &blc)
 	if err := p.Wait(); err != nil {
-		return nvim.ErrorWrap(v, errors.Annotate(err, pkgAstView))
+		err = errors.Annotate(err, pkgAstView)
+		return nvim.ErrorWrap(v, err)
 	}
 
-	var buf []byte
-	for _, b := range sources {
-		buf = append(buf, b...)
-		buf = append(buf, byte('\n'))
+	bufferLines, err := v.BufferLineCount(b)
+	if err != nil {
+		err = errors.Annotate(err, pkgAstView)
+		return nvim.ErrorWrap(v, err)
 	}
 
+	src := make([][]byte, bufferLines)
+	p.BufferLines(b, 0, -1, true, &src)
+	if err := p.Wait(); err != nil {
+		err = errors.Annotate(err, pkgAstView)
+		return nvim.ErrorWrap(v, err)
+	}
+
+	buf := bytes.Join(src, []byte{'\n'})
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, eval.File, buf, parser.AllErrors|parser.ParseComments)
 	if err != nil {
-		return nvim.ErrorWrap(v, errors.Annotate(err, pkgAstView))
+		err = errors.Annotate(err, pkgAstView)
+		return nvim.ErrorWrap(v, err)
 	}
 
 	_, file := filepath.Split(eval.File)
-	astInfo = append(astInfo, stringtoslicebyte(fmt.Sprintf("%s Files: %v\n", config.AstFoldIcon, file))...)
-	ast.Walk(VisitorFunc(parseAST), f)
+	viewer := &astViewer{data: stringtoslicebyte(fmt.Sprintf("%s Files: %v\n", config.AstFoldIcon, file))}
 
-	astinfo := bytes.Split(bytes.TrimSuffix(astInfo, []byte{'\n'}), []byte{'\n'})
-	if err := p.Wait(); err != nil {
-		return nvim.ErrorWrap(v, errors.Annotate(err, pkgAstView))
+	ast.Walk(VisitorFunc(viewer.parseAST), f)
+
+	data := buffer.ToBufferLines(v, bytes.TrimSuffix(viewer.data, []byte{'\n'}))
+
+	if astViewBuffer == nil {
+		bufOption := astViewOption("buffer")
+		bufVar := astViewVar("buffer")
+		winOption := astViewOption("window")
+		astViewBuffer = buffer.NewBuffer("__GoASTView__", fmt.Sprintf("silent belowright %d vsplit", config.TerminalWidth), int(config.TerminalWidth))
+		astViewBuffer.Create(v, bufOption, bufVar, winOption, nil)
+		astViewBuffer.UpdateSyntax(v, "goastview")
+
+		nnoremap := make(map[string]string)
+		nnoremap["q"] = ":<C-u>quit<CR>"
+		astViewBuffer.SetMapping(v, buffer.NoremapNormal, nnoremap)
+		p.Command("autocmd WinEnter <buffer> if winnr('$') == 1 | quit | endif")
 	}
 
-	var (
-		astBuf vim.Buffer
-		astWin vim.Window
-	)
+	v.SetBufferOption(astViewBuffer.Buffer, "modifiable", true)
+	defer v.SetBufferOption(astViewBuffer.Buffer, "modifiable", false)
 
-	p.Command("vertical botright 80 new")
-	p.CurrentBuffer(&astBuf)
-	p.CurrentWindow(&astWin)
+	p.SetBufferLines(astViewBuffer.Buffer, 0, -1, true, data)
 	if err := p.Wait(); err != nil {
-		return nvim.ErrorWrap(v, errors.Annotate(err, pkgAstView))
-	}
-
-	p.SetWindowOption(astWin, "number", false)
-	p.SetWindowOption(astWin, "list", false)
-	p.SetWindowOption(astWin, "colorcolumn", "")
-
-	p.SetBufferName(astBuf, "__GoAstView__")
-	p.SetBufferOption(astBuf, "modifiable", true)
-	p.SetBufferLines(astBuf, 0, -1, true, astinfo)
-	p.SetBufferOption(astBuf, "buftype", "nofile")
-	p.SetBufferOption(astBuf, "bufhidden", "delete")
-	p.SetBufferOption(astBuf, "buflisted", false)
-	p.SetBufferOption(astBuf, "swapfile", false)
-	p.SetBufferOption(astBuf, "modifiable", false)
-	p.SetBufferOption(astBuf, "filetype", "goastview")
-	p.Command("runtime! syntax/goastview.vim")
-	if err := p.Wait(); err != nil {
-		return nvim.ErrorWrap(v, errors.Annotate(err, pkgAstView))
+		err = errors.Annotate(err, pkgAstView)
+		return nvim.ErrorWrap(v, err)
 	}
 
 	p.SetCurrentWindow(w)
@@ -131,32 +126,61 @@ func (f VisitorFunc) Visit(n ast.Node) ast.Visitor {
 	return f(n)
 }
 
-func parseAST(node ast.Node) ast.Visitor {
+func (a *astViewer) parseAST(node ast.Node) ast.Visitor {
 	switch node := node.(type) {
 
 	default:
-		return VisitorFunc(parseAST)
+		return VisitorFunc(a.parseAST)
 	case *ast.Ident:
 		info := fmt.Sprintf("%s *ast.Ident\n\t Name: %v\n\t NamePos: %v\n", config.AstFoldIcon, node.Name, node.NamePos)
 		if fmt.Sprint(node.Obj) != "<nil>" {
 			info += fmt.Sprintf("\t Obj: %v\n", node.Obj)
 		}
-		astInfo = append(astInfo, stringtoslicebyte(info)...)
-		return VisitorFunc(parseAST)
+		a.data = append(a.data, stringtoslicebyte(info)...)
+		return VisitorFunc(a.parseAST)
 	case *ast.GenDecl:
-		astInfo = append(astInfo,
+		a.data = append(a.data,
 			stringtoslicebyte(fmt.Sprintf("%s Decls: []ast.Decl\n\t TokPos: %v\n\t Tok: %v\n\t Lparen: %v\n",
 				config.AstFoldIcon, node.TokPos, node.Tok, node.Lparen))...)
-		return VisitorFunc(parseAST)
+		return VisitorFunc(a.parseAST)
 	case *ast.BasicLit:
-		astInfo = append(astInfo,
+		a.data = append(a.data,
 			stringtoslicebyte(fmt.Sprintf("\t- Path: *ast.BasicLit\n\t\t\t Value: %v\n\t\t\t Kind: %v\n\t\t\t ValuePos: %v\n",
 				node.Value, node.Kind, node.ValuePos))...)
-		return VisitorFunc(parseAST)
+		return VisitorFunc(a.parseAST)
 
 	}
 }
 
-func stringtoslicebyte(s string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&s))
+func astViewOption(scope string) map[string]interface{} {
+	options := make(map[string]interface{})
+
+	switch scope {
+	case "buffer":
+		options[buffer.Bufhidden] = buffer.BufhiddenDelete
+		options[buffer.Buflisted] = false
+		options[buffer.Buftype] = buffer.BuftypeNofile
+		options[buffer.Filetype] = buffer.FiletypeAST
+		options[buffer.OpModifiable] = false
+		options[buffer.Swapfile] = false
+		options[buffer.OpModifiable] = false
+	case "window":
+		options[buffer.List] = false
+		options[buffer.Number] = false
+		options[buffer.Relativenumber] = false
+		options[buffer.Winfixheight] = false
+	}
+
+	return options
+}
+
+func astViewVar(scope string) map[string]interface{} {
+	vars := make(map[string]interface{})
+
+	switch scope {
+	case "buffer":
+		vars[buffer.Colorcolumn] = ""
+	}
+
+	return vars
 }
