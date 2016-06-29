87e990050c11f2992eebd459730538538e175d70
Koichi Shiraishi <k@zchee.io>
2016-06-29 15:50:11 +0900

    nvim/client: Add new Neovim client wrapper uses socket, embed, stdio
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

---
+++ src/nvim-go/nvim/client.go
@@ -0,0 +1,83 @@
+package nvim
+
+import (
+	"log"
+	"net"
+	"nvim-go/config"
+	"os"
+	"regexp"
+
+	"github.com/garyburd/neovim-go/vim"
+)
+
+// NewSocketClient creates the Neovim client over the socket session.
+func NewSocketClient() *vim.Vim {
+	var (
+		v   *vim.Vim
+		err error
+	)
+
+	addr := config.ServerName
+	if addr == "" {
+		return nil
+	}
+
+	v, err = dialNvim(addr)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return v
+}
+
+// NewEmbedClient creates the Neovim client over the embed api.
+func NewEmbedClient(args []string, dir string, env []string) *vim.Vim {
+	options := &vim.EmbedOptions{
+		Args: args,
+		Dir:  dir,
+		Env:  env,
+	}
+
+	v, err := vim.StartEmbeddedVim(options)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return v
+}
+
+// NewStdioClient creates the Neovim client over the stdio.
+func NewStdioClient() *vim.Vim {
+	v, err := vim.New(os.Stdin, os.Stdout, log.Printf)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	go v.Serve()
+
+	return v
+}
+
+var tcpAddrRe = regexp.MustCompile(`:\d+$`)
+
+func dialNvim(addr string) (*vim.Vim, error) {
+	network := "unix"
+	if tcpAddrRe.MatchString(addr) {
+		network = "tcp"
+	}
+
+	conn, err := net.Dial(network, addr)
+	if err != nil {
+		return nil, err
+	}
+	v, err := vim.New(conn, conn, log.Printf)
+	if err != nil {
+		return nil, err
+	}
+	go v.Serve()
+
+	return v, nil
+}
