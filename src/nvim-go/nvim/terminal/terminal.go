3a3e6fad9182343572accbb3cecf6855d4ea3a92
Koichi Shiraishi <k@zchee.io>
2016-06-29 12:34:27 +0900

    terminal: Add Terminal.Pipeline and Fix small things
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/nvim/terminal/terminal.go
+++ src/nvim-go/nvim/terminal/terminal.go
@@ -14,35 +14,37 @@ import (
 	"nvim-go/pathutil"
 
 	"github.com/garyburd/neovim-go/vim"
+	"github.com/juju/errors"
 )
 
 var pkgTerminal = "GoTerminal"
 
 var bufName = "__GO_TERMINAL__"
 
-// Terminal configure of open the terminal.
+// Terminal represents a Neovim terminal.
 type Terminal struct {
-	v    *vim.Vim
+	v *vim.Vim
+	p *vim.Pipeline
+
 	cmd  []string
 	mode string
+	// Name terminal buffer name.
 	Name string
 	// Dir specifies the working directory of the command on terminal.
 	Dir string
-	// Width split window width for open the terminal window.
-	Width int64
-	// Height split window height for open the terminal window.
-	Height int64
-	Size   int
+	// Size open the terminal window size.
+	Size int
 
-	cb     vim.Buffer
-	cw     vim.Window
-	Buffer *nvim.Buffer
+	cw vim.Window
+
+	*nvim.Buf
 }
 
-// NewTerminal return the initialize Neovim terminal config.
+// NewTerminal return the Neovim terminal buffer.
 func NewTerminal(vim *vim.Vim, name string, command []string, mode string) *Terminal {
 	return &Terminal{
 		v:    vim,
+		p:    vim.NewPipeline(),
 		Name: name,
 		cmd:  command,
 		mode: mode,
@@ -50,53 +52,47 @@ func NewTerminal(vim *vim.Vim, name string, command []string, mode string) *Term
 }
 
 // Create creats the new Neovim terminal buffer.
-func (t *Terminal) Create() error {
-	p := t.v.NewPipeline()
-	p.CurrentBuffer(&t.cb)
-	p.CurrentWindow(&t.cw)
-	if err := p.Wait(); err != nil {
+func (t *Terminal) Create() (err error) {
+	t.cw, err = t.v.CurrentWindow()
+	if err != nil {
 		return err
 	}
 
-	t.Height = config.TerminalHeight
-	t.Width = config.TerminalWidth
-
 	switch {
-	case t.Height != int64(0) && t.mode == "split":
-		t.Size = int(t.Height)
-	case t.Width != int64(0) && t.mode == "vsplit":
-		t.Size = int(t.Width)
-	case strings.Index(t.mode, "split") == -1:
-		return fmt.Errorf("%s mode is not supported", t.mode)
+	case t.mode == "split":
+		t.Size = int(config.TerminalHeight)
+	case t.mode == "vsplit":
+		t.Size = int(config.TerminalWidth)
+	default:
+		err := errors.Errorf("%s mode is not supported", t.mode)
+		return nvim.ErrorWrap(t.v, errors.Annotate(err, pkgTerminal))
 	}
 
 	option := t.setTerminalOption()
 	name := fmt.Sprintf("| terminal %s", strings.Join(t.cmd, " "))
 	mode := fmt.Sprintf("%s %d%s", config.TerminalPosition, t.Size, t.mode)
-	t.Buffer = nvim.NewBuffer(t.v, name, nvim.FiletypeTerminal, mode, option)
-	t.Buffer.Name = t.Name
-	t.Buffer.UpdateSyntax("")
+	t.Buf = nvim.NewBuffer(t.v, name, nvim.FiletypeTerminal, mode, option)
+	t.Buf.Name = t.Name
+	t.Buf.UpdateSyntax(nvim.FiletypeTerminal)
 
 	// Get terminal buffer and windows information.
-	p.CurrentBuffer(&t.Buffer.Buffer)
-	p.CurrentWindow(&t.Buffer.Window)
-	if err := p.Wait(); err != nil {
+	t.p.CurrentBuffer(&t.Buffer)
+	t.p.CurrentWindow(&t.Window)
+	if err := t.p.Wait(); err != nil {
 		return err
 	}
+	defer t.switchFocus()()
 
 	// Cleanup cursor highlighting
 	// TODO(zchee): Can use p.ClearBufferHighlight?
-	p.Command("highlight TermCursor gui=NONE guifg=NONE guibg=NONE")
-	p.Command("highlight TermCursorNC gui=NONE guifg=NONE guibg=NONE")
+	t.p.Command("highlight TermCursor gui=NONE guifg=NONE guibg=NONE")
+	t.p.Command("highlight TermCursorNC gui=NONE guifg=NONE guibg=NONE")
 
-	// Cleanup autocmd for terminal buffer
-	// The following autocmd is defined only in the terminal buffer local
-	p.Command("autocmd! * <buffer>")
 	// Set autoclose buffer if the current buffer is only terminal
 	// TODO(zchee): convert to rpc way
-	p.Command("autocmd WinEnter <buffer> if winnr('$') == 1 | quit | endif")
+	t.p.Command("autocmd WinEnter <buffer> if winnr('$') == 1 | quit | endif")
 
-	return p.Wait()
+	return t.p.Wait()
 }
 
 // Run runs the command in the terminal buffer.
@@ -105,14 +101,14 @@ func (t *Terminal) Run(cmd []string) error {
 		defer pathutil.Chdir(t.v, t.Dir)()
 	}
 
-	if t.Buffer != nil && nvim.BufContains(t.v, t.Buffer.Buffer) {
+	if t.Buf != nil && nvim.IsBufferValid(t.v, t.Buffer) {
 		defer t.switchFocus()()
 
-		t.v.SetBufferOption(t.Buffer.Buffer, nvim.BufOptionModified, false)
+		t.v.SetBufferOption(t.Buffer, nvim.BufOptionModified, false)
 		t.v.Call("termopen", nil, cmd)
+		t.v.SetBufferName(t.Buffer, t.Buf.Name)
 	} else {
 		t.Create()
-		defer t.switchFocus()()
 	}
 	// Workaround for "autocmd BufEnter term://* startinsert"
 	if config.TerminalStartInsert {
@@ -122,36 +118,9 @@ func (t *Terminal) Run(cmd []string) error {
 	return nil
 }
 
-func (t *Terminal) setTerminalOption() map[nvim.VimOption]map[string]interface{} {
-	option := make(map[nvim.VimOption]map[string]interface{})
-	bufoption := make(map[string]interface{})
-	bufvar := make(map[string]interface{})
-	windowoption := make(map[string]interface{})
-
-	bufoption[nvim.BufOptionBufhidden] = nvim.BufhiddenDelete
-	bufoption[nvim.BufOptionBuflisted] = false
-	bufoption[nvim.BufOptionBuftype] = nvim.BuftypeNofile
-	bufoption[nvim.BufOptionFiletype] = nvim.FiletypeTerminal
-	bufoption[nvim.BufOptionModifiable] = false
-	bufoption[nvim.BufOptionSwapfile] = false
-
-	bufvar[nvim.BufVarColorcolumn] = ""
-
-	windowoption[nvim.WinOptionList] = false
-	windowoption[nvim.WinOptionNumber] = false
-	windowoption[nvim.WinOptionRelativenumber] = false
-	windowoption[nvim.WinOptionWinfixheight] = true
-
-	option[nvim.BufferOption] = bufoption
-	option[nvim.BufferVar] = bufvar
-	option[nvim.WindowOption] = windowoption
-
-	return option
-}
-
 // TODO(zchee): flashing when switch the window.
 func (t *Terminal) switchFocus() func() {
-	t.v.SetCurrentWindow(t.Buffer.Window)
+	t.v.SetCurrentWindow(t.Window)
 
 	return func() {
 		t.v.SetCurrentWindow(t.cw)
@@ -175,4 +144,31 @@ func chdir(v *vim.Vim, dir string) func() {
 		v.ChangeDirectory(cwd.(string))
 		m.Unlock()
 	}
+}
+
+func (t *Terminal) setTerminalOption() map[nvim.NvimOption]map[string]interface{} {
+	option := make(map[nvim.NvimOption]map[string]interface{})
+	bufoption := make(map[string]interface{})
+	bufvar := make(map[string]interface{})
+	windowoption := make(map[string]interface{})
+
+	bufoption[nvim.BufOptionBufhidden] = nvim.BufhiddenDelete
+	bufoption[nvim.BufOptionBuflisted] = false
+	bufoption[nvim.BufOptionBuftype] = nvim.BuftypeNofile
+	bufoption[nvim.BufOptionFiletype] = nvim.FiletypeTerminal
+	bufoption[nvim.BufOptionModifiable] = false
+	bufoption[nvim.BufOptionSwapfile] = false
+
+	bufvar[nvim.BufVarColorcolumn] = ""
+
+	windowoption[nvim.WinOptionList] = false
+	windowoption[nvim.WinOptionNumber] = false
+	windowoption[nvim.WinOptionRelativenumber] = false
+	windowoption[nvim.WinOptionWinfixheight] = true
+
+	option[nvim.BufferOption] = bufoption
+	option[nvim.BufferVar] = bufvar
+	option[nvim.WindowOption] = windowoption
+
+	return option
 }
