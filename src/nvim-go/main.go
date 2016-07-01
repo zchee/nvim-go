0224fc4c54758d96c44d3c8eb03294e39dec7271
Koichi Shiraishi <k@zchee.io>
2016-07-01 09:53:21 +0900

    main: Add comments
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- src/nvim-go/main.go
+++ src/nvim-go/main.go
@@ -6,10 +6,14 @@
 package main
 
 import (
+	// For pprof debugging.
 	_ "net/http/pprof"
 
+	// Register autocmd
 	_ "nvim-go/autocmd"
+	// Register commands
 	_ "nvim-go/commands"
+	// Register delve command and autocmd
 	_ "nvim-go/commands/delve"
 
 	"github.com/garyburd/neovim-go/vim/plugin"
