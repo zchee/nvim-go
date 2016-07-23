be33289fb2f45ac198a984390665e1028da6825e
Koichi Shiraishi <zchee.io@gmail.com>
2016-07-23 19:46:10 +0900

    cmsd/helper: Rename StringToByteslice to ToByteSlice and Add test
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- src/nvim-go/commands/helper.go
+++ src/nvim-go/commands/helper.go
@@ -14,9 +14,9 @@ func ToPascalCase(s string) string { return strings.ToUpper(s[:1]) + s[1:] }
 // This function assumes that the character of the beginning is A-Z.
 func ToMixedCase(s string) string { return strings.ToLower(s[:1]) + s[1:] }
 
-// StringToByteslice convert string to byte slice use unsafe.
+// ToByteSlice convert string to byte slice use unsafe.
 // https://gist.github.com/dgryski/65d632958e4d88c7f79aaa7e1d2b10c0
-func StringToByteslice(s string) []byte {
+func ToByteSlice(s string) []byte {
 	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	bh := &reflect.SliceHeader{
 		Data: sh.Data,
---
+++ src/nvim-go/commands/helper_test.go
@@ -0,0 +1,82 @@
+package commands
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestToPascalCase(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "toPascalCase",
+			args: args{s: "toPascalCase"},
+			want: "ToPascalCase",
+		},
+		{
+			name: "ToPascalCas",
+			args: args{s: "ToPascalCase"},
+			want: "ToPascalCase",
+		},
+	}
+	for _, tt := range tests {
+		if got := ToPascalCase(tt.args.s); got != tt.want {
+			t.Errorf("%q. ToPascalCase(%v) = %v, want %v", tt.name, tt.args.s, got, tt.want)
+		}
+	}
+}
+
+func TestToMixedCase(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "toMixedCase",
+			args: args{s: "toMixedCase"},
+			want: "toMixedCase",
+		},
+		{
+			name: "ToMixedCase",
+			args: args{s: "ToMixedCase"},
+			want: "toMixedCase",
+		},
+	}
+	for _, tt := range tests {
+		if got := ToMixedCase(tt.args.s); got != tt.want {
+			t.Errorf("%q. ToMixedCase(%v) = %v, want %v", tt.name, tt.args.s, got, tt.want)
+		}
+	}
+}
+
+func TestToByteSlice(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "ToByteSlice",
+			args: args{s: "ToByteSlice"},
+			want: []byte("ToByteSlice"),
+		},
+	}
+	for _, tt := range tests {
+		if got := ToByteSlice(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. StringToByteslice(%v) = %v, want %v", tt.name, tt.args.s, got, tt.want)
+		}
+	}
+}
