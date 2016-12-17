b8faa60cb8ce0938952558f6ae4ecf6e5581aa5c
Koichi Shiraishi <zchee.io@gmail.com>
2016-12-17 18:29:50 +0900

    debug: add check the NVIM_GO_RACE env for use -race suffix binary
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -2,7 +2,7 @@
 
 touch $NVIM_GO_LOG_FILE
 
-if [[ -f $1/bin/nvim-go-race ]]; then
+if [[ -f $1/bin/nvim-go-race ]] && [[ -n "$NVIM_GO_RACE" ]]; then
   FLAG=-race
 fi
 exec $1/bin/nvim-go$FLAG 2>> $NVIM_GO_LOG_FILE
