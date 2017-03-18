a0bb334c47d6d92dc251c53bdce75b7206fff34d
Koichi Shiraishi <zchee.io@gmail.com>
2017-03-19 07:11:49 +0900

    script: fix if-else condition for '-race' suffix binary
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-touch $NVIM_GO_LOG_FILE
-
-if [[ -f $1/bin/nvim-go-race ]] && [[ -n "$NVIM_GO_RACE" ]]; then
-  FLAG=-race
+if [ -z "$NVIM_GO_LOG_FILE" ]; then
+  NVIM_GO_LOG_FILE=/tmp/nvim-go.log
 fi
-exec $1/bin/nvim-go$FLAG 2>> $NVIM_GO_LOG_FILE
+touch "$NVIM_GO_LOG_FILE"
+
+if [ -n "$NVIM_GO_RACE" ] && [ -f "$1/bin/nvim-go-race" ]; then
+  exec "$1/bin/nvim-go-race" 2>> "$NVIM_GO_LOG_FILE"
+else
+  exec "$1/bin/nvim-go" 2>> "$NVIM_GO_LOG_FILE"
+fi
