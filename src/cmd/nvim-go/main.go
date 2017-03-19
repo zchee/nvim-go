3d6a333a33d29a986af0a08c55e0680a9cf865ac
Koichi Shiraishi <zchee.io@gmail.com>
2017-03-19 20:47:25 +0900

    cmd/nvim-go: add log.SetFlags and move debugging section to register
    Can set stdlib log config if sets in the register variable
    
    TODO: Is move the gops & pprof debugging code to register is correct?
    
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- src/cmd/nvim-go/main.go
+++ src/cmd/nvim-go/main.go
@@ -23,31 +23,32 @@ import (
 
 func main() {
 	register := func(p *plugin.Plugin) error {
-		ctxt := context.NewContext()
+		log.SetFlags(log.Lshortfile)
 
+		ctxt := context.NewContext()
 		c := commands.Register(p, ctxt)
 		delve.Register(p, ctxt)
-
 		autocmd.Register(p, ctxt, c)
 
+		if os.Getenv("NVIM_GO_DEBUG") != "" {
+			// starts the gops agent
+			if err := agent.Listen(&agent.Options{NoShutdownCleanup: true}); err != nil {
+				return err
+			}
+
+			if os.Getenv("NVIM_GO_PPROF") != "" {
+				addr := "localhost:14715" // (n: 14)vim-(g: 7)(o: 15)
+				log.Printf("Start the pprof debugging, listen at %s\n", addr)
+
+				// enable the report of goroutine blocking events
+				runtime.SetBlockProfileRate(1)
+				go func() {
+					log.Println(http.ListenAndServe(addr, nil))
+				}()
+			}
+		}
+
 		return nil
-	}
-	if os.Getenv("NVIM_GO_DEBUG") != "" {
-		// starts the gops agent
-		if err := agent.Listen(&agent.Options{NoShutdownCleanup: true}); err != nil {
-			log.Fatal(err)
-		}
-
-		if os.Getenv("NVIM_GO_PPROF") != "" {
-			addr := "localhost:14715" // (n: 14)vim-(g: 7)(o: 15)
-			log.Printf("Start the pprof debugging, listen at %s\n", addr)
-
-			// enable the report of goroutine blocking events
-			runtime.SetBlockProfileRate(1)
-			go func() {
-				log.Println(http.ListenAndServe(addr, nil))
-			}()
-		}
 	}
 
 	plugin.Main(register)
