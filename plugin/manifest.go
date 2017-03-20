85782b1b7e9c087cce18f7faa793b42e2ac61c1c
Koichi Shiraishi <zchee.io@gmail.com>
2017-03-21 06:40:13 +0900

    plugin/manifest: fix problem that trim after manifest and refactoring
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- plugin/manifest.go
+++ plugin/manifest.go
@@ -20,20 +20,23 @@ import (
 )
 
 var (
-	old      = flag.Bool("old", false, "display old file data to stdout")
-	new      = flag.Bool("new", false, "display new file data to stdout")
-	manifest = flag.Bool("manifest", false, "display new plugin manifest to stdout")
-	write    = flag.Bool("w", false, "write specs to file instead of stdout")
+	old      = flag.Bool("old", false, "display old file data")
+	new      = flag.Bool("new", false, "display new file data")
+	manifest = flag.Bool("manifest", false, "display new plugin manifest")
+	write    = flag.Bool("w", false, "write plugin manifest to file instead of stdout")
 )
 
-func main() {
-	// Define usage
+var manifestRe = regexp.MustCompile(`(?s)call remote#host#RegisterPlugin.+\\ ]\)`)
+
+func init() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [-old|-new|-manifest|-w] plugin_name\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+}
 
+func main() {
 	// Required first arg of plugin_name
 	pluginName := flag.Arg(0)
 	if pluginName == "" || flag.NFlag() > 1 {
@@ -64,49 +67,43 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	newManifest = append(newManifest, '\n')
+	newManifest = bytes.TrimSuffix(newManifest, []byte{'\n'})
 
 	if strings.Contains(pluginName, "-race") {
 		pluginName = strings.TrimSuffix(pluginName, "-race")
 	}
 	// Get vim file information from the "./plugin" directory
-	plugFile, err := os.OpenFile(filepath.Join(prjDir, "plugin", pluginName+".vim"), os.O_RDWR, os.ModeAppend)
+	pluginFile, err := os.OpenFile(filepath.Join(prjDir, "plugin", pluginName+".vim"), os.O_RDWR)
 	if err != nil {
 		panic(err)
 	}
-	defer plugFile.Close()
+	defer pluginFile.Close()
 
 	// Read plugin vim file
-	oldData, err := ioutil.ReadAll(plugFile)
+	oldData, err := ioutil.ReadAll(pluginFile)
 	if err != nil {
 		panic(err)
 	}
 
-	re := regexp.MustCompile(`(?s)call remote#host#RegisterPlugin.+`)
 	// Replace the old specs to the latest specs
-	newData := re.ReplaceAll(oldData, newManifest)
+	newData := manifestRe.ReplaceAll(oldData, newManifest)
 
 	// Output result
 	switch {
 	case *old:
 		fmt.Printf("%v", string(oldData))
-		return
 	case *new:
 		fmt.Printf("%v", string(newData))
-		return
 	case *manifest:
 		// Trim last newline for output to stdout
-		fmt.Printf("%v", string(newManifest[:len(newManifest)-1]))
-		return
+		fmt.Printf("%v", string(newManifest))
 	case *write:
 		data := bytes.TrimSpace(newData)
 		if bytes.Contains(data, []byte("-race")) {
 			data = bytes.Replace(data, []byte("-race"), nil, -1)
 		}
-		data = append(data, byte('\n'))
-		if _, err := plugFile.WriteAt(data, 0); err != nil {
+		if _, err := pluginFile.WriteAt(data, 0); err != nil {
 			panic(err)
 		}
-		return
 	}
 }
