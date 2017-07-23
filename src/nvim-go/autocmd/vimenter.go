08d62a77bc62843917402935e215af39da3a8b90
Koichi Shiraishi <zchee.io@gmail.com>
2017-07-24 00:28:01 +0900

    autocmd: add merge config from setting file
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- src/nvim-go/autocmd/vimenter.go
+++ src/nvim-go/autocmd/vimenter.go
@@ -4,11 +4,20 @@
 
 package autocmd
 
-import "nvim-go/config"
+import (
+	"nvim-go/config"
+	"nvim-go/internal/log"
+)
 
 // VimEnter gets user config variables and assign to global variable when autocmd VimEnter.
 func (a *Autocmd) VimEnter(cfg *config.Config) {
 	cfg.Global.ChannelID = a.Nvim.ChannelID()
 
 	config.Get(a.Nvim, cfg)
+	cfg2, err := config.Read()
+	if err != nil {
+		log.Debugln(err)
+	}
+	// log.DebugDump(cfg2)
+	config.Merge(cfg, cfg2)
 }
