33bb9c68a7be5e7879034450aa860dd54bfd1964
Koichi Shiraishi <zchee.io@gmail.com>
2017-12-31 17:03:07 +0900

    cmd/nvim-go: rough implements childFn logic

--- cmd/nvim-go/main.go
+++ cmd/nvim-go/main.go
@@ -7,9 +7,9 @@ package main
 
 import (
 	"context"
-	"log"
+	logpkg "log"
 	"net/http"
-	_ "net/http/pprof" // For pprof debugging.
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"runtime"
@@ -21,29 +21,25 @@ import (
 	"github.com/zchee/nvim-go/src/buildctx"
 	"github.com/zchee/nvim-go/src/command"
 	"github.com/zchee/nvim-go/src/logger"
+	"github.com/zchee/nvim-go/src/server"
 	"go.uber.org/zap"
-)
-
-const (
-	envDebug = "NVIM_GO_DEBUG"
-	envPprof = "NVIM_GO_PPROF"
-)
-
-var (
-	debug = os.Getenv(envDebug) != ""
-	pprof = os.Getenv(envPprof) != ""
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
+	debug := os.Getenv("NVIM_GO_DEBUG") != ""
+	pprof := os.Getenv("NVIM_GO_PPROF") != ""
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	zapLogger := logger.NewZapLogger()
-	undo := zap.RedirectStdLog(zapLogger)
+	zapLogger, undo := logger.NewRedirectZapLogger()
 	defer undo()
 	ctx = logger.NewContext(ctx, zapLogger)
 
 	registerFn := func(p *plugin.Plugin) error {
+		log := logger.FromContext(ctx)
+
 		buildctxt := buildctx.NewContext()
 		c := command.Register(ctx, p, buildctxt)
 		autocmd.Register(ctx, p, buildctxt, c)
@@ -56,17 +52,57 @@ func main() {
 
 			if pprof {
 				const addr = "localhost:14715" // (n: 14)vim-(g: 7)(o: 15)
-				zapLogger.Debug("start the pprof debugging", zap.String("listen at", addr))
+				log.Debug("start the pprof debugging", zap.String("listen at", addr))
 
 				// enable the report of goroutine blocking events
 				runtime.SetBlockProfileRate(1)
-				go log.Println(http.ListenAndServe(addr, nil))
+				go logpkg.Println(http.ListenAndServe(addr, nil))
 			}
 		}
 
 		return nil
 	}
-	go plugin.Main(registerFn)
+
+	childFn := func(ctx context.Context) {
+		log := logger.FromContext(ctx)
+
+		cs, err := server.NewServer(ctx)
+		if err != nil {
+			log.Error("", zap.Error(err))
+		}
+		defer cs.Close()
+
+		bufs, err := cs.Nvim.Buffers()
+		if err != nil {
+			log.Error("", zap.Error(err))
+		}
+
+		// Get the names using a single atomic call to Nvim.
+		names := make([]string, len(bufs))
+		b := cs.Nvim.NewBatch()
+		for i, buf := range bufs {
+			b.BufferName(buf, &names[i])
+		}
+		if err := b.Execute(); err != nil {
+			log.Error("", zap.Error(err))
+		}
+		for _, name := range names {
+			log.Info("", zap.String("name", name))
+		}
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		plugin.Main(registerFn)
+		return nil
+	})
+	eg.Go(func() error {
+		childFn(ctx)
+		return nil
+	})
+	if err := eg.Wait(); err != nil {
+		zapLogger.Fatal("eg.Wait", zap.Error(err))
+	}
 
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
