5ef3cbeec1cd7d87de74e744305e0cb612146b5a
Koichi Shiraishi <zchee.io@gmail.com>
2018-01-02 21:30:28 +0900

    autocmd: add debug log and detail comment for Neovim autocmd behavior

--- src/autocmd/autocmd.go
+++ src/autocmd/autocmd.go
@@ -51,8 +51,27 @@ func Register(pctx context.Context, p *plugin.Plugin, buildContext *buildctx.Con
 		errs:             new(syncmap.Map),
 	}
 
+	autocmd.log.Debug("Register")
+
+	// Handle the open the file.
+	// If open the file at first, run the 'BufEnter' -> 'VimEnter'.
 	p.HandleAutocmd(&plugin.AutocmdOptions{Event: "BufEnter", Pattern: "*.go", Group: "nvim-go", Eval: "*"}, autocmd.BufEnter)
-	p.HandleAutocmd(&plugin.AutocmdOptions{Event: "BufWritePost", Pattern: "*.go", Group: "nvim-go", Eval: "*"}, autocmd.bufWritePost)
-	p.HandleAutocmd(&plugin.AutocmdOptions{Event: "BufWritePre", Pattern: "*.go", Group: "nvim-go", Eval: "*"}, autocmd.bufWritePre)
+
+	// Handle the initial start Neovim process.
+	// Note that does not run the 'VimEnter' handler if open the *not* go file. Because 'VimEnter' handler already run the other file or directory.
+	// TODO(zchee): consider Pattern to '*' instead of '*.go' with get '&filetype' and early return
 	p.HandleAutocmd(&plugin.AutocmdOptions{Event: "VimEnter", Pattern: "*.go", Group: "nvim-go", Eval: "*"}, autocmd.VimEnter)
+
+	// Handle the before the read to file.
+	// If create the new file, does not run the 'BufReadPre', Instead of 'BufNewFile'.
+	// p.HandleAutocmd(&plugin.AutocmdOptions{Event: "BufReadPre", Group: "nvim-go-autocmd", Pattern: "*.go"}, autocmd.BufReadPre)
+
+	// Handle create the new file.
+	// p.HandleAutocmd(&plugin.AutocmdOptions{Event: "BufNewFile", Group: "nvim-go-autocmd", Pattern: "*.go"}, autocmd.BufReadPre)
+
+	// Handle the before the write to file.
+	p.HandleAutocmd(&plugin.AutocmdOptions{Event: "BufWritePre", Pattern: "*.go", Group: "nvim-go", Eval: "*"}, autocmd.bufWritePre)
+
+	// Handle the after the write to file.
+	p.HandleAutocmd(&plugin.AutocmdOptions{Event: "BufWritePost", Pattern: "*.go", Group: "nvim-go", Eval: "*"}, autocmd.bufWritePost)
 }
