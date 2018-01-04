0cb629869b2a4ed4c461b8666593e6eed52cc8af
Koichi Shiraishi <zchee.io@gmail.com>
2018-01-05 03:40:05 +0900

    server: add retrying Dial connection

--- src/server/server.go
+++ src/server/server.go
@@ -21,29 +21,49 @@ type Server struct {
 }
 
 func NewServer(pctx context.Context) (*Server, error) {
-	const envNvimListenAddress = "NVIM_LISTEN_ADDRESS"
+	log := logger.FromContext(pctx).Named("server")
 
+	const envNvimListenAddress = "NVIM_LISTEN_ADDRESS"
 	addr := os.Getenv(envNvimListenAddress)
 	if addr == "" {
 		return nil, errors.Errorf("%s not set", envNvimListenAddress)
 	}
 
 	zapLogf := func(format string, a ...interface{}) {
-		logger.FromContext(pctx).Named("server").Info("", zap.Any(format, a))
+		log.Info("", zap.Any(format, a))
 	}
 
-	ctx, cancel := context.WithTimeout(pctx, 5*time.Second)
+	ctx, cancel := context.WithTimeout(pctx, 1*time.Second)
 	defer cancel()
 
-	n, err := nvim.Dial(addr, nvim.DialContext(ctx), nvim.DialServe(false), nvim.DialLogf(zapLogf))
-	if err != nil {
-		return nil, err
-	}
+	var n *nvim.Nvim
+	var tempDelay time.Duration
+	for {
+		var err error
+		n, err = nvim.Dial(addr, nvim.DialContext(ctx), nvim.DialServe(false), nvim.DialLogf(zapLogf))
+		if err != nil {
+			if tempDelay == 0 {
+				tempDelay = 5 * time.Millisecond
+			} else {
+				tempDelay *= 2
+			}
+			if max := 1 * time.Second; tempDelay > max {
+				tempDelay = max
+			}
+			log.Info("Dial error", zap.Error(err), zap.Duration("retrying in", tempDelay))
+			timer := time.NewTimer(tempDelay)
+			select {
+			case <-timer.C:
+			}
+			continue
+		}
+		tempDelay = 0
 
-	return &Server{
-		Nvim: n,
-		errc: make(chan error, 1),
-	}, nil
+		return &Server{
+			Nvim: n,
+			errc: make(chan error, 1),
+		}, nil
+	}
 }
 
 func (s *Server) Serve() {
