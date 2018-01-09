30bae18ac634971ef77e96ef4fe06fbc500e0d7a
Koichi Shiraishi <zchee.io@gmail.com>
2018-01-10 05:21:35 +0900

    pathutil/vcs: add comma to end of document comment

--- src/pathutil/vcs.go
+++ src/pathutil/vcs.go
@@ -11,7 +11,7 @@ import (
 
 var vcsDirs = []string{".git", ".svn", ".hg"}
 
-// FindVCSRoot find package root path from arg path
+// FindVCSRoot find package root path from arg path.
 func FindVCSRoot(basedir string) string {
 	var foundVCSDir bool
 
