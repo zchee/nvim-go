bbb128d4dd0330bde22f22ef2d61d85fa241a8d2
Koichi Shiraishi <zchee.io@gmail.com>
2018-02-06 05:38:01 +0900

    autocmd: move config.Get to BufEnter with sync.Once

--- src/autocmd/bufenter.go
+++ src/autocmd/bufenter.go
@@ -5,9 +5,13 @@
 package autocmd
 
 import (
+	"sync"
 	"time"
 
+	"github.com/zchee/nvim-go/src/config"
+	"github.com/zchee/nvim-go/src/logger"
 	"github.com/zchee/nvim-go/src/nvimutil"
+	"go.uber.org/zap"
 )
 
 // bufEnterEval represents the current buffer number, windows ID and buffer files directory.
@@ -15,16 +19,24 @@ type bufEnterEval struct {
 	BufNr int    `eval:"bufnr('%')"`
 	WinID int    `eval:"win_getid()"`
 	Dir   string `eval:"expand('%:p:h')"`
+
+	Cfg *config.Config
 }
 
+var configOnce sync.Once
+
 // BufEnter gets the current buffer number, windows ID and set context from the directory structure on BufEnter autocmd.
-func (a *Autocmd) BufEnter(eval *bufEnterEval) error {
+func (a *Autocmd) BufEnter(eval *bufEnterEval) {
 	defer nvimutil.Profile(a.ctx, time.Now(), "BufEnter")
+
+	configOnce.Do(func() {
+		eval.Cfg.Global.ChannelID = a.Nvim.ChannelID()
+		config.Get(a.Nvim, eval.Cfg)
+		logger.FromContext(a.ctx).Debug("VimEnter", zap.Any("eval.Config", eval.Cfg))
+	})
 
 	a.getStatus(eval.BufNr, eval.WinID, eval.Dir)
 	if eval.Dir != "" && a.buildContext.PrevDir != eval.Dir {
 		a.buildContext.SetContext(eval.Dir)
 	}
-
-	return nil
 }
--- src/autocmd/vimenter.go
+++ src/autocmd/vimenter.go
@@ -8,25 +8,10 @@ import (
 	"time"
 
 	"github.com/zchee/nvim-go/src/config"
-	"github.com/zchee/nvim-go/src/logger"
 	"github.com/zchee/nvim-go/src/nvimutil"
-	"go.uber.org/zap"
 )
 
 // VimEnter gets user config variables and assign to global variable when autocmd VimEnter.
 func (a *Autocmd) VimEnter(cfg *config.Config) {
 	defer nvimutil.Profile(a.ctx, time.Now(), "VimEnter")
-
-	cfg.Global.ChannelID = a.Nvim.ChannelID()
-
-	config.Get(a.Nvim, cfg)
-	logger.FromContext(a.ctx).Debug("VimEnter", zap.Any("cfg", cfg))
-
-	cfg2, err := config.Read()
-	if err != nil {
-		logger.FromContext(a.ctx).Error("VimEnter", zap.Error(err))
-	}
-	config.Merge(cfg, cfg2)
-
-	a.buildContext.SetContext(a.buildContext.Dir)
 }
