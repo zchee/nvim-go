8a3026e25316cd50ba7ac35438014e7cb8f899a7
Koichi Shiraishi <zchee.io@gmail.com>
2018-09-11 00:08:21 +0900

    cmd/nvim-go: add version variable

---
+++ cmd/nvim-go/version.go
@@ -0,0 +1,19 @@
+// Copyright 2018 The nvim-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// tag is version of merpay-gateway-jp.
+//
+// This is used for GCP profiler Sentry error reporting and so on. tag is overridden using
+// `-X main.tag` during release builds.
+var tag string
+
+// gitCommit is commit hash of merpay-gateway-jp.
+//
+// This is used for GCP profiler Sentry error reporting and so on. gitCommit is overridden using
+// `-X main.gitCommit` during release builds.
+var gitCommit string
+
+var version = tag + "@" + gitCommit
