3093e48838a8f8ab79fa8b34f5cde06b4e97f71e
Koichi Shiraishi <zchee.io@gmail.com>
2018-12-09 05:14:39 +0900

    pkg/config: export Env struct

--- pkg/config/env.go
+++ pkg/config/env.go
@@ -11,14 +11,14 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-// env represents a environment variabels for nvim-go.
-type env struct {
+// Env represents a environment variabels for nvim-go.
+type Env struct {
 	GCPProjectID string `envconfig:"GCP_PROJECT_ID"`
 	Debug        bool   `envconfig:"DEBUG"`
 	LogLevel     string `envconfig:"LOG_LEVEL"`
 }
 
-func (e env) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+func (e Env) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	enc.AddString("gcp_project_id", e.GCPProjectID)
 	enc.AddBool("debug", e.Debug)
 	enc.AddString("log_level", e.LogLevel)
@@ -27,13 +27,13 @@ func (e env) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 }
 
 // e is the global env variable
-var e env
+var e Env
 
 // envOnce for run Process once.
 var envOnce sync.Once
 
 // Process populates the specified struct based on environment variables.
-func Process() env {
+func Process() Env {
 	envOnce.Do(func() {
 		envconfig.MustProcess("NVIM_GO", &e)
 	})
