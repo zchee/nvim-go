07717b59ec6d37ac3c9aed374659d27f85f0c227
Koichi Shiraishi <zchee.io@gmail.com>
2018-12-09 03:55:53 +0900

    pkg/autocmd: add handle `NvimGo` method event

--- pkg/autocmd/register.go
+++ pkg/autocmd/register.go
@@ -8,16 +8,23 @@ import (
 	"context"
 
 	"github.com/neovim/go-client/nvim/plugin"
+	"go.uber.org/zap"
+	"golang.org/x/exp/errors/fmt"
 	"golang.org/x/sync/syncmap"
 
 	"github.com/zchee/nvim-go/pkg/buildctxt"
 	"github.com/zchee/nvim-go/pkg/command"
 	"github.com/zchee/nvim-go/pkg/logger"
+	"github.com/zchee/nvim-go/pkg/nvimctx"
 )
 
 // Register registers autocmd to Neovim.
+//
+//  :help rpcrequest()
+//  :help rpcnotify()
 func Register(pctx context.Context, cancel func(), p *plugin.Plugin, buildContext *buildctxt.Context, cmd *command.Command) {
-	ctx := logger.NewContext(pctx, logger.FromContext(pctx).Named("autocmd"))
+	log := logger.FromContext(pctx).Named("autocmd")
+	ctx := logger.NewContext(pctx, log)
 
 	autocmd := &Autocmd{
 		ctx:              ctx,
@@ -34,6 +41,10 @@ func Register(pctx context.Context, cancel func(), p *plugin.Plugin, buildContex
 	// Note that does not run the 'VimEnter' handler if open the *not* go file. Because 'VimEnter' handler already run the other file or directory.
 	// TODO(zchee): consider Pattern to '*' instead of '*.go' with get '&filetype' and early return
 	// p.HandleAutocmd(&plugin.AutocmdOptions{Event: "VimEnter", Pattern: "*", Group: "nvim-go", Eval: "*"}, autocmd.VimEnter)
+
+	p.Handle(nvimctx.Method, func(event ...interface{}) {
+		log.Debug(fmt.Sprintf("handles %s", nvimctx.Method), zap.Any("event", event))
+	})
 
 	// Handle the open the file.
 	// If open the file at first, run the 'BufEnter' -> 'VimEnter'.
