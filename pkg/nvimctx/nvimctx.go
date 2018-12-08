3ffbec710f831574605f36351bc830ab4a7810e8
Koichi Shiraishi <zchee.io@gmail.com>
2018-12-09 03:48:08 +0900

    pkg/nvimctx: add nvimctx package

---
+++ pkg/nvimctx/nvimctx.go
@@ -0,0 +1,9 @@
+// Copyright 2018 The nvim-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package nvimctx
+
+const (
+	Method = "NvimGo"
+)
