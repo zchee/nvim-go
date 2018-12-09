b091706d397d3c91da1c4869309c4a189efae8ca
Koichi Shiraishi <zchee.io@gmail.com>
2018-12-09 13:08:32 +0900

    pkg/command: use stdlib sync.Map and remove unnecessary logger config

--- pkg/command/command.go
+++ pkg/command/command.go
@@ -6,28 +6,25 @@ package command
 
 import (
 	"context"
+	"sync"
 
 	"github.com/neovim/go-client/nvim"
-	"golang.org/x/sync/syncmap"
 
 	"github.com/zchee/nvim-go/pkg/buildctxt"
-	"github.com/zchee/nvim-go/pkg/logger"
 )
 
 // Command represents a nvim-go plugins commands.
 type Command struct {
 	Nvim         *nvim.Nvim
 	buildContext *buildctxt.Context
-	errs         *syncmap.Map
+	errs         *sync.Map
 }
 
 // NewCommand return the new Command type with initialize some variables.
 func NewCommand(ctx context.Context, v *nvim.Nvim, bctxt *buildctxt.Context) *Command {
-	ctx = logger.NewContext(ctx, logger.FromContext(ctx).Named("command"))
-
 	return &Command{
 		Nvim:         v,
 		buildContext: bctxt,
-		errs:         new(syncmap.Map),
+		errs:         new(sync.Map),
 	}
 }
