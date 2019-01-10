ec4fe4ef133919928d8202cc086edda6a155b91e
Koichi Shiraishi <zchee.io@gmail.com>
2019-01-10 12:35:21 +0900

    Makefile,hack/make: update Makefiles

--- Makefile
+++ Makefile
@@ -1,112 +1,100 @@
 # ----------------------------------------------------------------------------
 # global
 
+.DEFAULT_GOAL = manifest
 APP = nvim-go
+CMD = $(PKG)/cmd/$(APP)
+CGO_ENABLED = 1
+
+# ----------------------------------------------------------------------------
+# target
+
+# ----------------------------------------------------------------------------
+# include
+
+include hack/make/go.mk
+
+# ----------------------------------------------------------------------------
+# override
+
 
 # ---------------------------------------------------------------------------
 # target
 
 # manifest
 .PHONY: manifest
-manifest: build  ## Write plugin manifest for developer
+manifest: build  ## Write plugin manifest.
 	$(call target)
 	@$(CURDIR)/bin/${APP} -manifest ${APP} -location $(CURDIR)/plugin/nvim-go.vim
 
 .PHONY: manifest/race
 manifest/race: APP=nvim-race
-manifest/race: build/race manifest  ## Write plugin manifest for developer
+manifest/race: build/race manifest  ## Write plugin manifest with race prefix.
 	$(call target)
 
 .PHONY: manifest/dump
-manifest/dump: build  ## Dump plugin manifest
+manifest/dump: build  ## Dump plugin manifest.
 	$(call target)
 	@$(CURDIR)/bin/${APP} -manifest ${APP}
 
 
 # internal vendor
 .PHONY: vendor/guru/update
-vendor/guru/update:  ## Update the internal guru package
+vendor/guru/update:
 	$(call target)
-	sed -i 's|unused-packages|# unused-packages|' Gopkg.toml
-	dep ensure -v -update golang.org/x/tools
-	${RM} -r $(shell find ${PACKAGE_ROOT}/pkg/internal/guru -maxdepth 1 -type f -name '*.go' -not -name 'result.go')
-	cp ${PACKAGE_ROOT}/vendor/golang.org/x/tools/cmd/guru/*.go ${PACKAGE_ROOT}/pkg/internal/guru
-	sed -i "s|\t// TODO(adonovan): opt: parallelize.|\tbp.GoFiles = append(bp.GoFiles, bp.CgoFiles...)\n\n\0|" pkg/internal/guru/definition.go
-	sed -i 's| // import "golang.org/x/tools/cmd/guru"||' ./pkg/internal/guru/main.go
-	sed -i 's|# unused-packages|unused-packages|' Gopkg.toml
-	export DEP_REVISION=$(dep status -detail -f='{{range $$i, $$p := .Projects}}{{if eq $$p.ProjectRoot "golang.org/x/tools"}}{{$$p.Locked.Revision}}{{end}}{{end}}')
-	perl -i -0pe 's|  name = "golang.org/x/tools"\n  branch = "master"\n|  name = "golang.org/x/tools"\n  revision = "${DEP_REVISION}"\n|m' Gopkg.toml
-	dep ensure -v -vendor-only
-	perl -i -0pe 's|  name = "golang.org/x/tools"\n  revision = "${DEP_REVISION}"\n|  name = "golang.org/x/tools"\n  branch = "master"\n|m' Gopkg.toml
-	dep ensure -v -no-vendor
-	unset DEP_REVISION
+	@GO111MODULE=on go get -u -m -v golang.org/x/tools@master
+	printf "%s\\n\\n%s" 'package guru' 'import _ "golang.org/x/tools/cmd/guru"' > $(CURDIR)/pkg/internal/guru/hack.go
+	@GO111MODULE=on go mod vendor -v
+	${RM} -r $(shell find $(CURDIR)/pkg/internal/guru -maxdepth 1 -type f -name '*.go' -not -name 'result.go')
+	mv $(CURDIR)/vendor/golang.org/x/tools/cmd/guru/*.go $(CURDIR)/pkg/internal/guru
+	sed -i "s|\t// TODO(adonovan): opt: parallelize.|\tbp.GoFiles = append(bp.GoFiles, bp.CgoFiles...)\n\n\0|" $(CURDIR)/pkg/internal/guru/definition.go
+	sed -i 's| // import "golang.org/x/tools/cmd/guru"||' $(CURDIR)/pkg/internal/guru/main.go
 
 .PHONY: vendor/guru/rename
 vendor/guru/rename: vendor/guru/update
 	$(call target)
-	@echo -e "[INFO] Rename main to guru\\n"
-	grep "package main" ${PACKAGE_ROOT}/pkg/internal/guru/*.go -l | xargs sed -i 's/package main/package guru/'
-	@echo -e "[INFO] Add Result interface\\n"
-	sed -i "s|PrintPlain(printf printfFunc)|\0\n\n\tResult(fset *token.FileSet) interface{}|" ${PACKAGE_ROOT}/pkg/internal/guru/guru.go
-	@echo -e "[INFO] Export functions\\n"
-	grep "findPackageMember" ${PACKAGE_ROOT}/pkg/internal/guru/*.go -l | xargs sed -i 's/findPackageMember/FindPackageMember/'
-	grep "packageForQualIdent" ${PACKAGE_ROOT}/pkg/internal/guru/*.go -l | xargs sed -i 's/packageForQualIdent/PackageForQualIdent/'
-	grep "guessImportPath" ${PACKAGE_ROOT}/pkg/internal/guru/*.go -l | xargs sed -i 's/guessImportPath/GuessImportPath/'
-	@echo -e "[INFO] remove canonical custom import path from main.go\\n"
-	sed -i "s|package guru|\n// +build ignore\n\n\0|" ${PACKAGE_ROOT}/pkg/internal/guru/main.go
+	grep "package main" $(CURDIR)/pkg/internal/guru/*.go -l | xargs sed -i 's/package main/package guru/'
+	sed -i "s|PrintPlain(printf printfFunc)|\0\n\n\tResult(fset *token.FileSet) interface{}|" $(CURDIR)/pkg/internal/guru/guru.go
+	grep "findPackageMember" $(CURDIR)/pkg/internal/guru/*.go -l | xargs sed -i 's/findPackageMember/FindPackageMember/'
+	grep "packageForQualIdent" $(CURDIR)/pkg/internal/guru/*.go -l | xargs sed -i 's/packageForQualIdent/PackageForQualIdent/'
+	grep "guessImportPath" $(CURDIR)/pkg/internal/guru/*.go -l | xargs sed -i 's/guessImportPath/GuessImportPath/'
+	sed -i "s|package guru|\n// +build ignore\n\n\0|" $(CURDIR)/pkg/internal/guru/main.go
 
 .PHONY: vendor/guru
-vendor/guru: vendor/guru/update vendor/guru/rename
-	$(call target)
+vendor/guru: vendor/guru/update vendor/guru/rename  ## Updates the vendoring guru package into pkg/internal.
 
-.PHONY: vendor/x/tools/update
+.PHONY: vendor/x/internal/tools/update
 vendor/x/tools/internal/update:
-	@go get -u -v golang.org/x/tools/internal/...
+	@GO111MODULE=off go get -u -v golang.org/x/tools/internal/...
 
-.PHONY: vendor/x/tools/%
-vendor/x/tools/%:
-	mkdir -p ${PACKAGE_ROOT}/pkg/internal/$*
-	find ${PACKAGE_ROOT}/pkg/internal/$* -type f -name '*.go' -print -delete
-	find /Users/zchee/go/src/golang.org/x/tools/internal/$* -type f -name '*.go' -and -not -name '*_test.go' -exec cp {} ${PACKAGE_ROOT}/pkg/internal/$* \;
+.PHONY: vendor/x/tools/internal/%
+vendor/x/tools/internal/%:
+	mkdir -p $(CURDIR)/pkg/internal/$*
+	find $(CURDIR)/pkg/internal/$* -type f -name '*.go' -print -delete
+	find /Users/zchee/go/src/golang.org/x/tools/internal/$* -type f -name '*.go' -and -not -name '*_test.go' -exec cp {} $(CURDIR)/pkg/internal/$* \;
 
-.PHONY: vendor/x/tools
-vendor/x/tools: vendor/x/tools/update vendor/x/tools/fastwalk vendor/x/tools/gopathwalk vendor/x/tools/semver
-	sed -i "s|golang.org/x/tools/internal/fastwalk|github.com/zchee/nvim-go/pkg/internal/fastwalk|" ${PACKAGE_ROOT}/pkg/internal/gopathwalk/walk.go
+.PHONY: vendor/x/tools/internal
+vendor/x/tools/internal: vendor/x/tools/internal/update  ## Updates the vendoring golang.org/x/tools/internal packages into pkg/internal.
+	${MAKE} vendor/x/tools/internal/fastwalk vendor/x/tools/internal/gopathwalk
+	sed -i "s|golang.org/x/tools/internal/fastwalk|$(PKG)/pkg/internal/fastwalk|" $(CURDIR)/pkg/internal/gopathwalk/walk.go
 
 .PHONY: vendor/bytebufferpool/update
 vendor/bytebufferpool/update:
-	@go get -u -v github.com/valyala/bytebufferpool
+	@GO111MODULE=off go get -u -v github.com/valyala/bytebufferpool
 
 .PHONY: vendor/x/tools
-vendor/bytebufferpool: vendor/bytebufferpool/update
-	mkdir -p ${PACKAGE_ROOT}/pkg/internal/$(subst vendor/bytebuffer,,$@)
-	find ${PACKAGE_ROOT}/pkg/internal/$(subst vendor/bytebuffer,,$@) -type f -name '*.go' -print -delete
-	find /Users/zchee/go/src/github.com/valyala/bytebufferpool -type f -name '*.go' -and -not -name '*_test.go' -exec cp {} ${PACKAGE_ROOT}/pkg/internal/$(subst vendor/bytebuffer,,$@) \;
+vendor/bytebufferpool: vendor/bytebufferpool/update  ## Update vendoring valyala/bytebufferpool package into pkg/internal.
+	mkdir -p $(CURDIR)/pkg/internal/$(subst vendor/bytebuffer,,$@)
+	find $(CURDIR)/pkg/internal/$(subst vendor/bytebuffer,,$@) -type f -name '*.go' -print -delete
+	find $(GO_PATH)/src/github.com/$(subst vendor,valyala,$@) -type f -name '*.go' -and -not -name '*_test.go' -exec cp {} $(CURDIR)/pkg/internal/$(subst vendor/bytebuffer,,$@) \;
 
 
-.PHONY: docker
-docker: docker/test  ## Run the docker container test on Linux
+## miscellaneous
+
+.PHONY: test/container
+test/container: container/build  ## Run the package test into Linux container.
 	$(call target)
-
-.PHONY: docker/build
-docker/build:  ## Build the zchee/nvim-go docker container for testing on the Linux
-	$(call target)
-	docker image build --rm --progress=plain -t $(IMAGE_REGISTRY)/$(APP) .
-
-.PHONY: docker/build-nocache
-docker/build-nocache:  ## Build the zchee/nvim-go docker container for testing on the Linux without cache
-	$(call target)
-	docker image build --rm --no-cache --progress=plain -t $(IMAGE_REGISTRY)/$(APP) .
-
-.PHONY: docker/test
-docker/test: docker/build  ## Run the package test with docker container
-	$(call target)
-	docker container run --rm -it $(IMAGE_REGISTRY)/$(APP) go test -v ${GO_TEST_FLAGS} ${PACKAGES}
-
-# ----------------------------------------------------------------------------
-# include
-
-include hack/make/go.mk
+	docker container run --rm -it $(IMAGE_REGISTRY)/$(APP):$(VERSION:v%=%) $(GO_TEST) -v -race $(strip $(GOFLAGS)) -run=$(GO_TEST_FUNC) $(GO_TEST_PKGS)
 
 # ----------------------------------------------------------------------------
 # override
--- hack/make/go.mk
+++ hack/make/go.mk
@@ -4,70 +4,48 @@
 # base
 SHELL := /usr/bin/env bash
 GO_PATH := $(shell go env GOPATH)
-CGO_ENABLED ?= 1
 
 # pkg
 PKG := $(subst $(GO_PATH)/src/,,$(CURDIR))
 GO_PKGS := $(shell go list ./... | grep -v -e '.pb.go')
 GO_ABS_PKGS := $(shell go list -f '$(GO_PATH)/src/{{.ImportPath}}' ./... | grep -v -e '.pb.go')
 GO_TEST_PKGS := $(shell go list -f='{{if or .TestGoFiles .XTestGoFiles}}{{.ImportPath}}{{end}}' ./...)
+GO_VENDOR_PKGS := $(shell go list -f '{{if and (or .GoFiles .CgoFiles) (ne .Name "main")}}./vendor/{{.ImportPath}}{{end}}' ./vendor/...)
 
 # version
-VERSION=$(shell cat VERSION.txt || devel)
+VERSION=$(shell cat VERSION.txt)
 GIT_COMMIT := $(shell git rev-parse --short HEAD)
 GIT_UNTRACKED_CHANGES:= $(shell git status --porcelain --untracked-files=no)
 ifneq ($(GIT_UNTRACKED_CHANGES),)
 	GIT_COMMIT := $(GIT_COMMIT)-dirty
 endif
-# CTIMEVAR=-X=$(PKG)/main.tag=$(VERSION) -X=$(PKG)/main.gitCommit=$(GIT_COMMIT)
 CTIMEVAR=-X $(PKG)/pkg/version.Tag=$(VERSION) -X $(PKG)/pkg/version.GitCommit=$(GIT_COMMIT)
-# CTIMEVAR=-X $(PKG)/pkg/version.Version=$(VERSION)@$(GIT_COMMIT)
 
-# gcflags, ldflags
-GO_GCFLAGS?=
-GO_LDFLAGS=$(CTIMEVAR)
-GO_LDFLAGS_STATIC=$(CTIMEVAR) '-extldflags=-static'
-# GO_LDFLAGS_STATIC=${GO_LDFLAGS}+='-extldflags=-static'
-ifneq ($(NVIM_GO_DEBUG),)
-	GO_LDFLAGS+=-w -s
+CGO_ENABLED ?= 0
+GO_BUILD_TAGS := osusergo netgo
+GO_FLAGS ?= -tags '$(GO_BUILD_TAGS)'
+
+ifeq ($(NVIM_GO_DEBUG),)
+	GO_LDFLAGS:=-ldflags "-w -s $(CTIMEVAR)"
+	GO_LDFLAGS_STATIC:=-ldflags "-w -s $(CTIMEVAR) -extldflags -static"
 else
-	GO_GCFLAGS+=all='-N -l -dwarflocationlists=true'  # https://tip.golang.org/doc/diagnostics.html#debugging
-	GO_LDFLAGS+=-compressdwarf=false
+	GO_GCFLAGS:=-gcflags all='-N -l -dwarflocationlists=true'  # https://tip.golang.org/doc/diagnostics.html#debugging
+	GO_LDFLAGS:=-ldflags "compressdwarf=false $(CTIMEVAR)"
 endif
 
-# build tags
-GO_BUILD_TAGS ?= osusergo netgo
-
-# GOFLAGS
-GOFLAGS ?= -tags '$(GO_BUILD_TAGS)' -installsuffix netgo
-ifeq ($(CI),)
+ifneq ($(wildcard go.mod),)  # exist go.mod file
+ifeq ($(CI),)  # $CI is empty
 	GOFLAGS+=-mod=vendor
 endif
-
-ifneq ($(GO_GCFLAGS),)
-	GOFLAGS+=-gcflags $(strip $(GO_GCFLAGS))
-endif
-ifneq ($(GO_LDFLAGS),)
-	GOFLAGS+=-ldflags '$(strip $(GO_LDFLAGS))'
 endif
 
-# test
 GO_TEST ?= go test
 GO_TEST_FUNC ?= .
+GO_TEST_FLAGS ?=
 GO_BENCH_FUNC ?= .
+GO_BENCH_FLAGS ?= -benchmem
 
-# lint
-GOLANGCI_EXCLUDE ?=
-ifeq ($(wildcard '.errcheckignore'),)
-	GOLANGCI_EXCLUDE=$(foreach pat,$(shell cat .errcheckignore),--exclude '$(pat)')
-endif
-GOLANGCI_CONFIG ?=
-ifeq ($(wildcard '.golangci.yml'),)
-	GOLANGCI_CONFIG+=--config .golangci.yml
-endif
-
-# docker
-IMAGE_REGISTRY := quay.io/zchee
+IMAGE_REGISTRY := gcr.io/container-image
 
 # ----------------------------------------------------------------------------
 # defines
@@ -81,195 +59,132 @@ endef
 # targets
 
 .PHONY: bin/$(APP)
-bin/$(APP):
-	CGO_ENABLED=$(CGO_ENABLED) go build -v -o ./bin/${APP} $(strip $(GOFLAGS)) ./cmd/${APP}
+bin/$(APP): mod/vendor
+	$(call target,$@)
+	CGO_ENABLED=$(CGO_ENABLED) go build -v $(strip $(GOFLAGS)) -o $@ $(CMD)
 
 .PHONY: $(APP)
 $(APP): bin/$(APP)
 
 .PHONY: build
+build: GO_FLAGS+=$(GO_GCFLAGS) ${GO_LDFLAGS}
 build: $(APP)  ## Builds a dynamic executable or package.
-	$(call target)
 
 .PHONY: build/race
-build/race: GOFLAGS+=-race
-build/race: clean $(APP)  ## Build the nvim-go binary with race
+build/race: GO_FLAGS+=-race
+build/race: GO_FLAGS+=$(GO_GCFLAGS) ${GO_LDFLAGS}
+build/race: clean $(APP) mod/vendor  ## Build the nvim-go binary with race
 	$(call target)
 
 .PHONY: static
-static: GOFLAGS+=${GO_LDFLAGS_STATIC}
-static: $(APP)  ## Builds a static executable or package.
+static: GO_BUILD_TAGS+=static
+static: GO_FLAGS+=$(GO_GCFLAGS) ${GO_LDFLAGS_STATIC}
+static: $(APP) mod/vendor  ## Builds a static executable or package.
 	$(call target)
+
+.PHONY: install
+install: GO_FLAGS+=${GO_LDFLAGS_STATIC}
+install: mod/vendor  ## Installs the executable or package.
+	$(call target)
+	CGO_ENABLED=$(CGO_ENABLED) go install -a -v $(strip $(GO_FLAGS)) $(CMD)
 
 
 ## test, bench and coverage
 
 .PHONY: test
-test:  ## Run the package test with checks race condition.
+test: mod/vendor  ## Run the package test with checks race condition.
 	$(call target)
 	$(GO_TEST) -v -race $(strip $(GOFLAGS)) -run=$(GO_TEST_FUNC) $(GO_TEST_PKGS)
 
-# .PHONY: test/cpu
-# test/cpu: GOFLAGS+=-cpuprofile cpu.out
-# test/cpu: _test  ## Run the package test with take a cpu profile.
-# 	$(call target)
-#
-# .PHONY: test/mem
-# test/mem: GOFLAGS+=-memprofile mem.out
-# test/mem: _test  ## Run the package test with take a memory profile.
-# 	$(call target)
-#
-# .PHONY: test/mutex
-# test/mutex: GOFLAGS+=-mutexprofile mutex.out
-# test/mutex: _test  ## Run the package test with take a mutex profile.
-# 	$(call target)
-#
-# .PHONY: test/block
-# test/block: GOFLAGS+=-blockprofile block.out
-# test/block: _test  ## Run the package test with take a blockingh profile.
-# 	$(call target)
-#
-# .PHONY: test/trace
-# test/trace: GOFLAGS+=-trace trace.out
-# test/trace: _test  ## Run the package test with take a trace profile.
-# 	$(call target)
-
 .PHONY: bench
-bench:  ## Take a package benchmark.
+bench: mod/vendor  ## Take a package benchmark.
 	$(call target)
 	$(GO_TEST) -v $(strip $(GOFLAGS)) -run='^$$' -bench=$(GO_BENCH_FUNC) -benchmem $(GO_TEST_PKGS)
 
-# .PHONY: bench/cpu
-# bench/cpu: GOFLAGS+=-cpuprofile cpu.out
-# bench/cpu: bench  ## Take a package benchmark with take a cpu profile.
-#
-# .PHONY: bench/trace
-# bench/trace:  ## Take a package benchmark with take a trace profile.
-# 	$(call target)
-# 	$(GO_TEST) -v -c -o bench-trace.test $(PKG)/stackdriver
-# 	GODEBUG=allocfreetrace=1 ./bench-trace.test -test.run=none -test.bench=$(GO_BENCH_FUNC) -test.benchmem -test.benchtime=10ms 2> trace.log
+.PHONY: bench/race
+bench/race: mod/vendor  ## Take a package benchmark with checks race condition.
+	$(call target)
+	$(GO_TEST) -v -race $(strip $(GO_FLAGS)) -run='^$$' -bench=$(GO_BENCH_FUNC) -benchmem $(GO_TEST_PKGS)
+
+.PHONY: bench/trace
+bench/trace:  ## Take a package benchmark with take a trace profiling.
+	$(GO_TEST) -v -c -o bench-trace.test $(PKG)/stackdriver
+	GODEBUG=allocfreetrace=1 ./bench-trace.test -test.run=none -test.bench=$(GO_BENCH_FUNC) -test.benchmem -test.benchtime=10ms 2> trace.log
 
 .PHONY: coverage
-coverage:  ## Take test coverage.
+coverage: mod/vendor  ## Take test coverage.
 	$(call target)
 	$(GO_TEST) -v -race $(strip $(GOFLAGS)) -covermode=atomic -coverpkg=$(PKG)/... -coverprofile=coverage.out $(GO_TEST_PKGS)
 
+.PHONY: $(GO_PATH)/bin/go-junit-report
 $(GO_PATH)/bin/go-junit-report:
 	@GO111MODULE=off go get -u github.com/jstemmer/go-junit-report
 
-cmd/go-junit-report: $(GO_PATH)/bin/go-junit-report  ## go get `go-junit-report` binary.
+.PHONY: cmd/go-junit-report
+cmd/go-junit-report: $(GO_PATH)/bin/go-junit-report  # go get 'go-junit-report' binary
 
 .PHONY: coverage/junit
-coverage/junit: cmd/go-junit-report  ## Take test coverage and output test results with junit syntax.
+coverage/junit: cmd/go-junit-report mod/vendor  ## Take test coverage and output test results with junit syntax.
 	$(call target)
-	mkdir -p _test-results
-	$(GO_TEST) -v -race $(strip $(GOFLAGS)) -covermode=atomic -coverpkg=$(PKG)/... -coverprofile=coverage.out $(GO_TEST_PKGS) 2>&1 | tee /dev/stderr | go-junit-report -set-exit-code > _test-results/report.xml
+	@mkdir -p test-results
+	$(GO_TEST) -v -race $(strip $(GO_FLAGS)) -covermode=atomic -coverpkg=$(PKG)/... -coverprofile=coverage.out $(GO_PKGS) 2>&1 | tee /dev/stderr | go-junit-report -set-exit-code > test-results/report.xml
 
 
 ## lint
 
-lint: lint/fmt lint/govet lint/golint lint/golangci-lint  ## Run all linters.
+.PHONY: lint
+lint: lint/fmt lint/golangci-lint  ## Run all linters.
 
 .PHONY: lint/fmt
 lint/fmt:  ## Verifies all files have been `gofmt`ed.
 	$(call target)
-	@gofmt -s -l . | grep -v -e 'vendor' -e '.pb.go' | tee /dev/stderr
+	@gofmt -s -l . 2>&1 | grep -v -E -e 'testdata' -e 'vendor' -e '\.pb.go' -e '_.*' | tee /dev/stderr
 
-.PHONY: lint/govet
-lint/govet:  ## Verifies `go vet` passes.
-	$(call target)
-	@go vet -all $(GO_PKGS) | tee /dev/stderr
-
-$(GO_PATH)/bin/golint:
-	@GO111MODULE=off go get -u golang.org/x/lint/golint
-
-cmd/golint: $(GO_PATH)/bin/golint  ## go get `golint` binary.
-
-.PHONY: lint/golint
-lint/golint: cmd/golint  ## Verifies `golint` passes.
-	$(call target)
-	@golint -min_confidence=0.3 $(GO_PKGS)
-
+.PHONY: $(GO_PATH)/bin/golangci-lint
 $(GO_PATH)/bin/golangci-lint:
-	@GO111MODULE=off go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
+	@GO111MODULE=off go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
 
-cmd/golangci-lint: $(GO_PATH)/bin/golangci-lint  ## Install `golangci-lint` command.
+.PHONY: cmd/golangci-lint
+cmd/golangci-lint: $(GO_PATH)/bin/golangci-lint  # go get 'golangci-lint' binary
 
 .PHONY: golangci-lint
-lint/golangci-lint: cmd/golangci-lint  ## Run golangci-lint.
+lint/golangci-lint: cmd/golangci-lint .golangci.yml mod/vendor  ## Run golangci-lint.
 	$(call target)
-	@golangci-lint run $(strip $(GOLANGCI_CONFIG)) ./...
+	@golangci-lint run ./...
 
 
-## vendor
-
-### dep
-
-$(GO_PATH)/bin/dep:
-	@go get -u github.com/golang/dep/cmd/dep
-
-cmd/dep: $(GO_PATH)/bin/dep  ## go get `dep` binary.
-
-.PHONY: dep/init
-dep/init: cmd/dep  ## Init dep files.
-	$(call target)
-	@dep init -v -no-examples
-
-.PHONY: dep/ensure
-dep/ensure: cmd/dep Gopkg.toml  ## Fetchs the vendor packages via dep ensure.
-	$(call target)
-	@dep ensure -v
-
-.PHONY: dep/ensure/only-vendor
-dep/ensure/vendor: cmd/dep Gopkg.toml Gopkg.lock  ## Fetchs the vendor packages only via dep ensure.
-	$(call target)
-	@dep ensure -v -vendor-only
-
-.PHONY: dep/update
-dep/update: cmd/dep  ## Updates the vendor packages via dep.
-	$(call target)
-	@dep ensure -v -update
-
-.PHONY: dep/clean
-dep/clean: cmd/dep  ## Cleanups the dep vendoring tool files.
-	$(call target)
-	@$(RM) Gopkg.toml Gopkg.lock
-
-.PHONY: dep
-dep: dep/ensure  ## Updates all vendor packages via dep.
-
-### mod
+## mod
 
 .PHONY: mod/init
-mod/init:  ## Init go.mod file.
+mod/init:
 	$(call target)
 	@GO111MODULE=on go mod init
 
-.PHONY: mod/update
-mod/update:  ## Update module and go.mod.
+.PHONY: mod/goget
+mod/goget:  ## Update module and go.mod.
 	$(call target)
 	@GO111MODULE=on go get -u -m -v -x ./...
 
 .PHONY: mod/tidy
-mod/tidy:  ## Makes sure go.mod matches the source code in the module.
+mod/tidy:
 	$(call target)
 	@GO111MODULE=on go mod tidy -v
 
 .PHONY: mod/vendor
-mod/vendor: go.mod go.sum  ## Fetchs the vendor packages via go mod.
+mod/vendor:
 	$(call target)
-	@GO111MODULE=on go mod vendor -v
+	@GO111MODULE=on go mod vendor
 
 .PHONY: mod/graph
-mod/graph:  ## Prints the module requirement graph with replacements applied in text form.
+mod/graph:
 	$(call target)
 	@GO111MODULE=on go mod graph
 
 .PHONY: mod/clean
-mod/clean:  ## Cleanups the go mod vendoring tool files.
+mod/clean:
 	$(call target)
 	@$(RM) go.mod go.sum
-
+	@$(RM) -r vendor
 
 .PHONY: mod/lock/go-client
 mod/lock/go-client:  # locked to neovim/go-client@api/32405de
@@ -282,27 +197,47 @@ mod/lock/delve:  # locked to derekparker/delve@92dad94
 	@go get -u -m -v -x github.com/derekparker/delve@92dad94 golang.org/x/arch@f40095975f golang.org/x/debug@fb508927b4 golang.org/x/sys@f3918c30c5
 
 .PHONY: mod
-mod: mod/clean mod/init mod/tidy  ## Updates the vendor packages via go mod.
+mod: mod/clean mod/init mod/lock/go-client mod/lock/delve mod/tidy mod/vendor  ## Updates the vendoring directory via go mod.
 	@sed -i ':a;N;$$!ba;s|go 1\.12\n\n||g' go.mod
 
-.PHONY: vendor
-vendor: mod/lock/go-client mod/lock/delve mod/tidy mod/vendor  ## Ensure tidy and fetch depends package to vendor directory.
+.PHONY: mod/install
+mod/install: mod/lock/go-client mod/lock/delve mod/tidy mod/vendor
+	$(call target)
+	@GO111MODULE=off go install -v $(GO_VENDOR_PKGS) || GO111MODULE=on go install -mod=vendor -v $(GO_VENDOR_PKGS)
 
-.PHONY: vendor/install
-vendor/install:
-	@go install -v $(shell go list -f '{{if and (or .GoFiles .CgoFiles) (ne .Name "main")}}{{.ImportPath}}{{end}}' ./vendor/...)
-
-.PHONY: vendor/update
-vendor/update: mod/update mod/lock/go-client mod/lock/delve mod/tidy mod/vendor vendor/install  ## Updates all vendor packages.
+.PHONY: mod/update
+mod/update: mod/goget mod/lock/go-client mod/lock/delve mod/tidy mod/vendor mod/install  ## Updates all vendor packages.
 
 
 ## miscellaneous
 
+.PHONY: container/build
+container/build: Dockerfile mod/vendor  ## Build the zchee/nvim-go docker container for testing on the Linux.
+	docker image build --rm --force-rm --pull --progress=plain -t $(IMAGE_REGISTRY)/$(APP):$(VERSION:v%=%) .
+
+.PHONY: container/build/nocache
+container/build/nocache:  ## Build the zchee/nvim-go docker container for testing on the Linux without cache.
+	$(call target)
+	docker image build --rm --force-rm --no-cache --pull --progress=plain -t -t $(IMAGE_REGISTRY)/$(APP):$(VERSION:v%=%) .
+
+.PHONY: container/push
+container/push:  ## Push the container image to $IMAGE_REGISTRY.
+	docker image push $(IMAGE_REGISTRY)/$(APP):$(VERSION:v%=%)
+
+
+.PHONY: boilerplate/go/%
 boilerplate/go/%: BOILERPLATE_PKG_DIR=$(shell printf $@ | cut -d'/' -f3- | rev | cut -d'/' -f2- | rev)
 boilerplate/go/%: BOILERPLATE_PKG_NAME=$(if $(findstring $@,cmd),main,$(shell printf $@ | rev | cut -d/ -f2 | rev))
-boilerplate/go/%: hack/boilerplate/boilerplate.go.txt  ## Create initial .go file from the boilerplate.go.txt
+boilerplate/go/%: hack/boilerplate/boilerplate.go.txt  ## Creates go file to % location based from boilerplate.go.txt.
 	@if [ ! -d ${BOILERPLATE_PKG_DIR} ]; then mkdir -p ${BOILERPLATE_PKG_DIR}; fi
 	@cat hack/boilerplate/boilerplate.go.txt <(printf "package ${BOILERPLATE_PKG_NAME}\\n") > $*
+	@sed -i "s|YEAR|$(shell date '+%Y')|g" $*
+
+
+.PHONY: clean
+clean:  ## Cleanups the any build binaries or packages.
+	$(call target)
+	@$(RM) -r ./bin *.out *.test *.prof trace.log
 
 
 .PHONY: AUTHORS
@@ -312,15 +247,9 @@ AUTHORS:  ## Creates AUTHORS file.
 	@printf "$(shell git log --format="\n%aN <%aE>" | LC_ALL=C.UTF-8 sort -uf)" >> $@
 
 
-.PHONY: clean
-clean:  ## Cleanups the any build binaries or packages.
-	$(call target)
-	@$(RM) -r ./bin *.out *.test *.prof trace.log
-
-
 .PHONY: todo
 todo:  ## Print the all of (TODO|BUG|XXX|FIXME|NOTE) in nvim-go package sources
-	@pt -e '(TODO|BUG|XXX|FIXME|NOTE)(\(.+\):|:)' --follow --hidden --ignore=.git --ignore=vendor --ignore=internal --ignore=Makefile --ignore=snippets --ignore=indent
+	@rg -e '(TODO|BUG|XXX|FIXME|NOTE)(\(.+\):|:)' --follow --hidden --glob='!.git' --glob='!vendor' --glob='!internal' --glob='!Makefile' --glob='!snippets' --glob='!indent'
 
 
 .PHONY: help
