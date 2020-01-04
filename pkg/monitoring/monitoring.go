48cd76f456e2a6dd5c64ec24950eebdcdab0cd32
Koichi Shiraishi <zchee.io@gmail.com>
2020-01-04 23:30:34 +0900

    pkg/monitoring: remove nctx

--- pkg/monitoring/monitoring.go
+++ pkg/monitoring/monitoring.go
@@ -6,13 +6,10 @@ package monitoring
 
 import (
 	"context"
-	"path"
 
 	"go.opencensus.io/trace"
-
-	"github.com/zchee/nvim-go/pkg/nctx"
 )
 
 func StartSpan(pctx context.Context, name string) (context.Context, *trace.Span) {
-	return trace.StartSpan(pctx, path.Join(nctx.PkgName, name))
+	return trace.StartSpan(pctx, name)
 }
